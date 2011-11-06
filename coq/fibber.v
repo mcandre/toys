75ba922cd701b113a8ff1d50c512f8b1f2305e0b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-11-06 01:23:17 -0400

    simplified

--- coq/fibber.v
+++ coq/fibber.v
@@ -17,13 +17,6 @@ end.
 Definition one : nat := S O.
 Definition two : nat := S(S O).
 
-Fixpoint minus (n m : nat) : nat :=
-match n, m with
-| O, _ => O
-| S _, O => n
-| S n', S m' => minus n' m'
-end.
-
 Fixpoint fibber (n : nat) : nat :=
 match n with
 | O => O
