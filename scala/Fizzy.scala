e0e3d35d5eaa7ac9d0db098688b459b9b83e1d05
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-11-13 00:33:53 -0500

    added FizzBuzz in Scala

---
+++ scala/Fizzy.scala
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+exec scala "$0" "$@"
+!#
+
+object Fizzy {
+	def fizzy() {
+		(1 to 100).map { i =>
+			if (i % 15 == 0)
+				"FizzBuzz"
+			else if (i % 3 == 0)
+				"Fizz"
+			else if (i % 5 == 0)
+				"Buzz"
+			else
+				i.toString()
+		}.map { s =>
+			println(s)
+		}
+	}
+
+	def main(args: Array[String]) {
+		fizzy()
+	}
+}
