fa229318be9bac38fab93c5361415e68c2f9b4d1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-11-19 18:22:36 -0500

    added FizzBuzz

---
+++ groovy/fizzy.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+def fizzy = { n ->
+	if (n % 15 == 0) {
+		"FizzBuzz"
+	}
+	else if (n % 3 == 0) {
+		"Fizz"
+	}
+	else if (n % 5 == 0) {
+		"Buzz"
+	}
+	else {
+		n.toString()
+	}
+}
+
+(1 .. 100).collect(fizzy).each({ s -> println s })
