a8f547a47adff79fc5bd5b4062d5293c1131cd01
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-02-07 12:38:54 -0500

    added Fibonacci

---
+++ rust/fib.rs
@@ -0,0 +1,19 @@
+#[link(name = "fib")];
+
+use std;
+
+fn fib(n : int) -> int {
+	if (n == 0) {
+		ret 0;
+	}
+	else if (n == 1) {
+		ret 1;
+	}
+	else {
+		ret fib(n - 1) + fib(n - 2);
+	}
+}
+
+fn main() {
+	std::io::println("Fib 40 = " + core::int::to_str(fib(40), 10u));
+}
