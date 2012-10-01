628ce3c01b7e49a1a60a2cc0deb4c22f7cf22e70
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-01 10:20:27 -0400

    forth Hello World nasm gitignore

--- nasm/hello/hello.asm
+++ nasm/hello/hello.asm
@@ -1,17 +1,28 @@
+[bits 32]
+
 section .data
 
-msg: db "Hello World!"
+msg: db "Hello World!", 0
 
 section .text
 
-global main
+global start
 extern puts
 
-main:
+start:
+
+; setup stack frame
+push ebp
+mov ebp, esp
 
 push msg
 call puts
+add esp, 4
 
-leave
+; tear down stack frame
+mov esp, ebp
+pop ebp
+
 mov eax, 0
+leave
 ret
