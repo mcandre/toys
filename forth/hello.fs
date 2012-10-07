ef96cccc25fa68b22d4bfcf74c94e9a87364aeef
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-07 18:23:45 -0400

    shebang Hello World nasm/windows working

--- forth/hello.fs
+++ forth/hello.fs
@@ -1,2 +1,4 @@
+#! /usr/bin/env gforth
+
 .\" Hello World!\n"
 bye
---
+++ nasm/hello/windows/hello.asm
@@ -0,0 +1,35 @@
+[bits 32]
+
+section .data
+
+stdout equ -11
+null equ 0
+
+msg: db "Hello World!", 13, 10, 0
+len equ $-msg
+
+section .bss
+
+dummy resd 1
+
+section .text
+
+global Start
+extern GetStdHandle
+extern WriteConsoleA
+extern ExitProcess
+
+Start:
+
+push stdout
+call GetStdHandle
+
+push null
+push dummy
+push len
+push msg
+push eax
+call WriteConsoleA
+
+push null
+call ExitProcess
