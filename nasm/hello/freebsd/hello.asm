c92765c0a894430e91a231df48d7ccfc2149580a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-07 19:44:21 -0400

    Hello World nasm/MacOSX working

---
+++ nasm/hello/freebsd/hello.asm
@@ -0,0 +1,26 @@
+[bits 32]
+
+section .data
+
+msg db "Hello World!", 10
+len equ $-msg
+
+section .text
+
+global _start
+
+_start:
+
+push len
+push msg
+push 1		; stdout
+mov eax, 4	; sys_write
+sub esp, 4	; kernel
+int 0x80
+
+add esp, 16	; clean stack (kernel + 4 bytes * 3 arguments)
+
+push 0
+mov eax, 1	; sys_exit
+sub esp, 4	; kernel
+int 0x80
