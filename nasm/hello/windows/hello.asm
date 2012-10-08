5be94343e6c0b8a0c8da88261dc5acb6c798e1e6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-07 21:20:14 -0400

    idioms

--- nasm/hello/windows/hello.asm
+++ nasm/hello/windows/hello.asm
@@ -20,6 +20,7 @@ Start:
 
 push -11			; get stdout
 call GetStdHandle
+add esp, 4			; clear stack (4 * 1 argument)
 
 push 0				; null
 push buf			; [chars written]
@@ -27,6 +28,7 @@ push len
 push msg
 push eax			; stdout
 call WriteConsoleA
+add esp, 20			; clear stack (4 * 5 arguments)
 
 push 0
 call ExitProcess
