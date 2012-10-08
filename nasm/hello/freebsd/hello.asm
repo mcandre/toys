f7edf31d4450ffdbfdc7d088942d50734251a357
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-07 23:28:14 -0400

    clarity

--- nasm/hello/freebsd/hello.asm
+++ nasm/hello/freebsd/hello.asm
@@ -2,6 +2,13 @@
 
 section .data
 
+stdout equ 1
+
+sys_write equ 4
+sys_exit equ 1
+
+kernel equ 0x80
+
 msg db "Hello World!", 10
 len equ $-msg
 
@@ -13,13 +20,13 @@ start:
 
 push len
 push msg
-push 1		; stdout
-mov eax, 4	; sys_write
-sub esp, 4	; kernel
-int 0x80
+push stdout
+mov eax, sys_write
+sub esp, 4
+int kernel
 add esp, 16	; clean stack (kernel + 4 bytes * 3 arguments)
 
 push 0
-mov eax, 1	; sys_exit
-sub esp, 4	; kernel
-int 0x80
+mov eax, sys_exit
+sub esp, 4
+int kernel
