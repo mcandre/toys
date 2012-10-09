c3fbb35e02cdf90063a36c6d8c4e879f5cbb0edd
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-09 10:55:55 -0400

    FizzBuzz in Ruby

---
+++ ruby/fizzy.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+def fizz(n)
+	if n % 15 == 0
+		return "FizzBuzz"
+	elsif n % 3 == 0
+		return "Fizz"
+	elsif n % 5 == 0
+		return "Buzz"
+	else
+		return n.to_s
+	end
+end
+
+def main
+	1.upto(100) { |i| puts fizz(i) }
+end
+
+if __FILE__==$0
+	begin
+		main
+	rescue Interrupt => e
+		nil
+	end
+end
