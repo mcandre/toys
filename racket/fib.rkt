b57c739a16f64b7d2eba3f7b3a903641b44a6fc6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-15 11:56:25 -0400

    Fibonacci in Racket

---
+++ racket/fib.rkt
@@ -0,0 +1,12 @@
+#!/usr/bin/env racket
+#lang racket
+
+(provide fib)
+
+(define (fib n)
+	(cond
+		[(equal? n 0) 0]
+		[(equal? n 1) 1]
+		[else (+ (fib (- n 1)) (fib (- n 2)))]))
+
+(module+ main (printf "~a\n" (fib 30)))
