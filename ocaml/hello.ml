b58930707b22cb176d1300f6e12a8a93b0c3deb3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-11-11 11:55:10 -0500

    idioms

--- ocaml/hello.ml
+++ ocaml/hello.ml
@@ -1,1 +1,8 @@
-print_endline "Hello World!"
+if true then ignore begin let kkkk _ _ _ _ = 0 in kkkk
+"exec" "ocaml" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
+then exit
+fi
+true else 0
+end;;
+
+let main = print_endline "Hello World!"
