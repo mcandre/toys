7209b1402e04c6ed9243e803aa14b9825cfcd27f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-12-09 11:57:18 -0500

    resolving Fay errors

--- fay/random.hs
+++ fay/random.hs
@@ -3,6 +3,33 @@ module Random where
 import Language.Fay.FFI
 import Language.Fay.Prelude
 
+data Element
+instance Foreign Element
+instance Show (Element)
+
 -- Monad returning a uniform random integer from [0,m)
-random :: Integer -> Fay Int
-random = ffi "Math.random() * %1"
+random :: Int -> Fay Int
+random = ffi "Math.floor(Math.random() * %1)"
+
+addEventListener :: Element -> String -> Fay () -> Bool -> Fay ()
+addEventListener = ffi "%1.addEventListener(%1,%2,%3)"
+
+getElementsByTagName :: String -> Fay [Element]
+getElementsByTagName = ffi "document.getElementsByTagName(%1)"
+
+getFirstElementByTagName :: String -> Fay Element
+getFirstElementByTagName tag = ffi "document.getElementsByTagName(%1)[0]"
+
+setText :: Element -> String -> Fay ()
+setText = ffi "%1.text = %2"
+
+refresh :: Fay ()
+refresh = do
+	d <- getFirstElementByTagName "div"
+	r <- random 100
+	setText d $ show r
+
+main :: Fay ()
+main = do
+	body <- getFirstElementByTagName "body"
+	addEventListener body "load" refresh False
