9ebd357922e725c6086d7effad26e4c324f622b2
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-02-25 13:25:29 -0500

    fewer flops and parallelized

--- ruby/fizzy.rb
+++ ruby/fizzy.rb
@@ -1,19 +1,24 @@
 #!/usr/bin/env ruby
 
+require "rubygems"
+require "parallel"
+
 def fizz(n)
-	if n % 15 == 0
-		return "FizzBuzz"
-	elsif n % 3 == 0
-		return "Fizz"
-	elsif n % 5 == 0
-		return "Buzz"
-	else
-		return n.to_s
-	end
+  x, y = n % 3, n % 5
+
+  if [x, y] == [0, 0]
+    "FizzBuzz"
+  elsif x == 0
+    "Fizz"
+  elsif y == 0
+    "Buzz"
+  else
+    n.to_s
+  end
 end
 
 def main
-	1.upto(100) { |i| puts fizz(i) }
+  Parallel.map(1.upto(100)) { |i| fizz(i) }.each { |s| puts s }
 end
 
 main if __FILE__ == $0
