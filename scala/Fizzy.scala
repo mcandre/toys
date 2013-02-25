886cde01a532bfabee0d7534c6f65e6d0e4becc9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-02-25 17:14:14 -0500

    fewer flops and parallelized

--- scala/Fizzy.scala
+++ scala/Fizzy.scala
@@ -2,14 +2,12 @@
 
 object Fizzy {
 	def fizzy {
-		(1 to 100).map { n =>
-			val x, y = (n % 3, n % 5)
-
-			(x, y) match {
+		(1 to 100).par.map { n =>
+			(n % 3, n % 5) match {
 				case (0, 0) => "FizzBuzz"
 				case (0, _) => "Fizz"
 				case (_, 0) => "Buzz"
-				case _ => n.toString
+				case (_, _) => n.toString
 			}
 		}.map(println)
 	}
