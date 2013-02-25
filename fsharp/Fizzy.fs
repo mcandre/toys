72cbf744e78a7146ffa51d0e829549ca851ab032
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-02-25 14:13:15 -0500

    debugging Fizzy.fs

---
+++ fsharp/Fizzy.fs
@@ -0,0 +1,17 @@
+namespace Fizzy
+
+module Fizzy =
+  let fizzbuzz n =
+    let x, y  = (n % 3, n % 5)
+
+    if (x, y) = (0, 0) then
+      "FizzBuzz"
+    else if x = 0 then
+      "Fizz"
+    else if y = 0 then
+      "Buzz"
+    else
+      n.ToString("d")
+
+  let main =
+    Array.iter (fun s -> printfn "%s" s) (Array.Parallel.collect fizzbuzz [1 .. 101])
