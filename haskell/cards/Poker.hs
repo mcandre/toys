1f100de3dfd739c208b9d2773ba664e3c5d11699
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-03-11 13:59:41 -0400

    poker

---
+++ haskell/cards/Poker.hs
@@ -0,0 +1,22 @@
+data Suit
+     = Spade
+     | Heart
+     | Diamond
+     | Club
+     deriving (Eq, Ord, Show, Read)
+
+data Rank
+     = Ace
+     | Two
+     | Three
+     | Four
+     | Five
+     | Six
+     | Seven
+     | Eight
+     | Nine
+     | Ten
+     | Jack
+     | Queen
+     | King
+     deriving (Eq, Ord, Show, Read)
