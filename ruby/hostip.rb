fcc259e915a4c799a523586b85b8e611d3f1cac2
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-05 18:03:29 -0400

    reek

--- ruby/hostip.rb
+++ ruby/hostip.rb
@@ -22,75 +22,69 @@ require "rdoc/usage"
 require "open-uri"
 
 def prepare_api(settings)
-	url="http://#{settings[:domain]}#{settings[:api]}"
-	url+="&ip=#{settings[:ip]}" if settings[:ip].length>0
-	url+="&position=true" if settings[:position]
+  url = "http://#{settings[:domain]}#{settings[:api]}"
+  url += "&ip=#{settings[:ip]}" if settings[:ip].length > 0
+  url += "&position=true" if settings[:position]
 
-	return url
+  url
 end
 
 def geoip(settings)
-	url=prepare_api(settings)
+  url = prepare_api(settings)
 
-	begin
-		open(url) { |f|
-			return f.readlines
-		}
-	rescue Timeout::Error=>e
-		raise "Unable to access #{url}"
-	rescue
-		raise "Unable to access #{url}"
-	end
+  begin
+    open(url) { |f| f.readlines }
+  rescue
+    raise "Unable to access #{url}"
+  end
 end
 
 def main
-	settings={
-		:domain => "api.hostip.info",
-		:api => "/get_html.php?",
-		:ip => "",
-		:position => false
-	}
+  settings = {
+    :domain => "api.hostip.info",
+    :api => "/get_html.php?",
+    :ip => "",
+    :position => false
+  }
 
-	begin
-		opts=GetoptLong.new(
-			["--help", "-h", GetoptLong::NO_ARGUMENT],
-			["--address", "-a", GetoptLong::REQUIRED_ARGUMENT],
-			["--position", "-p", GetoptLong::NO_ARGUMENT]
-		)
+  begin
+    opts=GetoptLong.new(
+      ["--help", "-h", GetoptLong::NO_ARGUMENT],
+      ["--address", "-a", GetoptLong::REQUIRED_ARGUMENT],
+      ["--position", "-p", GetoptLong::NO_ARGUMENT]
+    )
 
-		opts.each { |option, value|
-			case option
-			when "--help"
-				raise
-			when "--address"
-				settings[:ip]=value
-			when "--position"
-				settings[:position]=true
-			end
-		}
-	rescue
-		RDoc::usage("Usage")
-	end
+    opts.each { |option, value|
+      case option
+      when "--help"
+        raise
+      when "--address"
+        settings[:ip] = value
+      when "--position"
+        settings[:position] = true
+      end
+    }
+  rescue
+    RDoc::usage("Usage")
+  end
 
-	args=[]+ARGV
-	args << "" if args.length==0
+  args = [] + ARGV
+  args << "" if args.length == 0
 
-	args.each { |ip|
-		settings[:ip]=ip
-		begin
-			puts geoip(settings)
-		rescue SocketError => e
-			puts "Could not connect to #{settings[:domain]}"
-		rescue RuntimeError => e
-			puts e.message
-		end
-	}
+  args.each { |ip|
+    settings[:ip] = ip
+    begin
+      puts geoip(settings)
+    rescue SocketError => e
+      puts "Could not connect to #{settings[:domain]}"
+    end
+  }
 end
 
-if __FILE__==$0
-	begin
-		main
-	rescue Interrupt => e
-		nil
-	end
+if __FILE__ == $0
+  begin
+    main
+  rescue Interrupt => e
+    nil
+  end
 end
