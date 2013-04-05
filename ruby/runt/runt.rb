4a8b7839608278ca055778d42776d352baa72868
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-05 18:33:10 -0400

    style

--- ruby/runt/runt.rb
+++ ruby/runt/runt.rb
@@ -2,15 +2,15 @@
 
 require "webrick"
 
-server=WEBrick::HTTPServer.new(
-	:MaxClients => 100,
-	:MimeTypes => WEBrick::HTTPUtils::DefaultMimeTypes,
-	:DirectoryIndex => ["index.html","index.htm","index.cgi","index.rhtml"],
-	:BindAddress => "localhost",
-	:Port => 8080,
-	:DocumentRoot => File.dirname($0)+"/"+"www/",
-	:AccessLog => [] # suppress log messages
-)
+server = WEBrick::HTTPServer.new(
+  :MaxClients => 100,
+  :MimeTypes => WEBrick::HTTPUtils::DefaultMimeTypes,
+  :DirectoryIndex => ["index.html","index.htm","index.cgi","index.rhtml"],
+  :BindAddress => "localhost",
+  :Port => 8080,
+  :DocumentRoot => File.dirname($0)+"/"+"www/",
+  :AccessLog => [] # suppress log messages
+                               )
 
 trap("INT") { server.shutdown }
 
--- ruby/sitestatus.rb
+++ ruby/sitestatus.rb
@@ -3,33 +3,29 @@
 require "open-uri"
 
 def site_status(domain)
-	data=nil
+  data = nil
 
-	begin
-		data=open("http://www.iwebtool.com/tool/tools/server_status/server_status.php?domain=#{domain}").read
-	rescue Timeout::Error=>e
-		return "unknown"
-	rescue
-		return "unknown"
-	end
+  begin
+    data = open("http://www.iwebtool.com/tool/tools/server_status/server_status.php?domain=#{domain}").read
+  rescue
+    return "unknown"
+  end
 
-	if not data["The website is accessible!"].nil?
-		return "up"
-	else
-		return "down"
-	end
+  if not data["The website is accessible!"].nil?
+    "up"
+  else
+    "down"
+  end
 end
 
 def main
-	ARGV.each { |arg|
-		puts "#{arg}: #{site_status(arg)}"
-	}
+  ARGV.each { |arg| puts "#{arg}: #{site_status(arg)}" }
 end
 
-if __FILE__==$0
-	begin
-		main
-	rescue Interrupt => e
-		nil
-	end
+if __FILE__ == $0
+  begin
+    main
+  rescue Interrupt => e
+    nil
+  end
 end
