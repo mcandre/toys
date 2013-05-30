1f98c53493d08ab796ab3820e2ece7956c19befe
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 19:35:29 -0400

    lint

--- python/beer.py
+++ python/beer.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
-if __name__=="__main__":
-	for i in range(99, 1, -1):
-		print("%d bottles of beer on the wall, %d bottles of beer.\nTake one down, pass it around, %d bottles of beer on the wall.\n" % (i, i, i-1))
+"""99 bottles of beer song"""
 
-	print("1 bottle of beer on the wall, 1 bottle of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+if __name__ == "__main__":
+  for i in range(99, 1, -1):
+    print(
+      "" + i +
+      " bottles of beer on the wall, " + i +
+      " bottles of beer.\n" +
+      "Take one down, pass it around, " + (i - 1) +
+      " bottles of beer on the wall.\n"
+    )
+
+  print(
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall."
+  )
