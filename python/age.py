1f3fcc0e11bc50969cb16a298a7a6178f9e31b49
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 19:27:45 -0400

    lint

--- python/age.py
+++ python/age.py
@@ -2,18 +2,18 @@
 
 """ Takes an age and prints corresponding results """
 
-if __name__=="__main__":
-	age=int(input("Age: "))
+if __name__ == "__main__":
+  AGE = int(raw_input("Age: "))
 
-	if age<1:
-		print("You are too young!")
-	elif age in range(1,13):
-		print("You are a child.")
-	elif age in range(13,30):
-		print("You are young.")
-	elif age in range(30,100):
-		print("You are an adult. Act like it.")
-	elif age in range(100, 125):
-		print("That is OLD!")
-	else:
-		print("Possible error. Too old/young?")
+  if AGE < 1:
+    print("You are too young!")
+  elif AGE in range(1, 13):
+    print("You are a child.")
+  elif AGE in range(13, 30):
+    print("You are young.")
+  elif AGE in range(30, 100):
+    print("You are an adult. Act like it.")
+  elif AGE in range(100, 125):
+    print("That is OLD!")
+  else:
+    print("Possible error. Too old/young?")
