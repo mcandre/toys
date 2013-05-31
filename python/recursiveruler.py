7f5a3613d492ea3780c3719e2dd56a2d46902863
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-31 10:36:29 -0400

    lint

--- python/recursiveruler.py
+++ python/recursiveruler.py
@@ -1,85 +1,89 @@
 #!/usr/bin/env python
 
-__author__="Andrew Pennebaker (andrew.pennebaker@gmail.com)"
-__date__="23 Jan 2007 - 6 Feb 2007"
-__copyright__="Copyright 2007 Andrew Pennebaker"
+"""Ruler drawer"""
 
-import sys
+__author__ = "Andrew Pennebaker (andrew.pennebaker@gmail.com)"
+__date__ = "23 Jan 2007 - 6 Feb 2007"
+__copyright__ = "Copyright 2007 Andrew Pennebaker"
 
 import pygame
+import sys
 
-def drawRuler(screen, color, start, max, min, parts):
-	screenrect=screen.get_rect()
+def draw_ruler(screen, color, start, max, min, parts):
+  """Blot ruler"""
 
-	width, height=screenrect.width, screenrect.height
+  screenrect = screen.get_rect()
 
-	top, bottom=0, height
-	left, right=0, width
+  width, height = screenrect.width, screenrect.height
 
-	for i in range(parts-1):
-		markerrect=pygame.Rect(start-(width/parts)*i, bottom-max, 1, max)
+  bottom = height
 
-		screen.fill(color, markerrect)
+  for i in range(parts-1):
+    markerrect = pygame.Rect(start - (width / parts) * i, bottom - max, 1, max)
+    screen.fill(color, markerrect)
 
-	for i in range(parts-1):
-		markerrect=pygame.Rect(start+(width/parts)*i, bottom-max, 1, max)
+  for i in range(parts - 1):
+    markerrect = pygame.Rect(start + (width / parts) * i, bottom - max, 1, max)
+    screen.fill(color, markerrect)
 
-		screen.fill(color, markerrect)
+  parts *= 2
+  max /= 2
 
-	parts*=2
-	max=max/2
+  if int(max) > min:
+    draw_ruler(screen, color, start, max, min, parts)
 
-	if int(max)>min:
-		drawRuler(screen, color, start, max, min, parts)
+def play(screen, screensync = True, fps = 128):
+  """Tick"""
 
-def play(screen, screensync=True, fps=128):
-	black=(0, 0, 0)
-	red=(255, 0, 0)
+  black = (0, 0, 0)
+  red = (255, 0, 0)
 
-	screenrect=screen.get_rect()
-	width, height=screenrect.width, screenrect.height
+  screenrect = screen.get_rect()
+  height = screenrect.height
 
-	clock=pygame.time.Clock()
+  clock = pygame.time.Clock()
 
-	while True:
-		for event in pygame.event.get():
-			if event.type==pygame.QUIT:
-				sys.exit()
-			elif event.type==pygame.KEYDOWN:
-				if event.key==pygame.K_ESCAPE:
-					sys.exit()
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+          sys.exit()
 
-		mousepos=pygame.mouse.get_pos()
+    mousepos = pygame.mouse.get_pos()
 
-		screen.fill(black)
-		drawRuler(screen, red, mousepos[0], height-mousepos[1], 1, 2)
+    screen.fill(black)
+    draw_ruler(screen, red, mousepos[0], height - mousepos[1], 1, 2)
 
-		if screensync:
-			clock.tick(fps)
+    if screensync:
+      clock.tick(fps)
 
-		pygame.display.flip()
+    pygame.display.flip()
 
 def main():
-	pygame.init()
-	pygame.mouse.set_visible(False)
+  """Show ruler"""
 
-	resolution=(1024, 768)
-	fullscreen=True
+  pygame.init()
+  pygame.mouse.set_visible(False)
 
-	screensync=True
-	fps=128
+  resolution = (1024, 768)
+  fullscreen = True
 
-	screen=None
+  screensync = True
+  fps = 128
 
-	if fullscreen:
-		screen=pygame.display.set_mode(resolution, pygame.FULLSCREEN)
-	else:
-		screen=pygame.display.set_mode(resolution)
+  screen = None
 
-	play(screen, screensync, fps)
+  if fullscreen:
+    screen = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
+  else:
+    screen = pygame.display.set_mode(resolution)
 
-if __name__=="__main__":
-	try:
-		main()
-	except KeyboardInterrupt, e:
-		pass
+  play(screen, screensync, fps)
+
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
