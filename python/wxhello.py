76849c9669c2e47ed63c1d5b38ef9a5b4ff0b815
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-31 17:18:42 -0400

    lint

--- python/wxhello.py
+++ python/wxhello.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python
 
+"""Wx demo"""
+
 import wx
 
-ID_ANY=wx.ID_ANY
-ID_HELLO=110
+ID_ANY = wx.ID_ANY
+ID_HELLO = 110
 
 class HelloFrame(wx.Frame):
-	def __init__(self, parent, id, title, pos=wx.DefaultPosition, size=(150, 75)):
-		wx.Frame.__init__(self, parent, id, title, pos, size)
+  """Hello frame"""
 
-		self.button=wx.Button(self, ID_HELLO, "Hello World", wx.DefaultPosition, wx.Size(20, 10))
+  def __init__(
+      self,
+      parent,
+      id,
+      title,
+      pos = wx.DefaultPosition, size = (150, 75)
+  ):
+    wx.Frame.__init__(self, parent, id, title, pos, size)
 
-		wx.EVT_BUTTON(self, ID_HELLO, self.OnHello)
+    self.button = wx.Button(
+      self,
+      ID_HELLO,
+      "Hello World",
+      wx.DefaultPosition,
+      wx.Size(20, 10)
+    )
 
-		self.Show(True)
+    wx.EVT_BUTTON(self, ID_HELLO, self.on_hello)
 
-	def OnHello(self, event):
-		print "Hello World"
+    self.Show(True)
 
-if __name__=="__main__":
-	app=wx.PySimpleApp()
-	frame=HelloFrame(None, ID_ANY, "wxHello")
-	app.MainLoop()
+  def on_hello(self, event):
+    """Hello click"""
+    print "Hello World"
+
+def main():
+  """GUI"""
+
+  app = wx.PySimpleApp()
+  HelloFrame(None, ID_ANY, "wxHello")
+  app.MainLoop()
+
+if __name__ == "__main__":
+  main()
