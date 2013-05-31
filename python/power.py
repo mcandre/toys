9309fc2741bb877571dcacbf1df1450bc7e670ba
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-31 10:23:28 -0400

    lint

--- python/power.py
+++ python/power.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+"""2^n"""
+
+def main():
+  """CLI"""
+
+  n = int(raw_input("N: "))
+
+  print("Running")
+
+  x = 2**n
+
+  print("2^n=", x)
+
 if __name__ == "__main__":
-	n = int(input("N: "))
-
-	print("Running")
-
-	x = 2**n
-
-	print("2^n=", x)
+  main()
