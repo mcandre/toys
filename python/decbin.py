f39e50022a0ec5e368c221f298f56488c8b7f0e1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 20:32:26 -0400

    lint

--- python/decbin.py
+++ python/decbin.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 
-"""Converts decimal numbers to binary using division. Thanks go to Eric Nygren (nygrene@stcsc.edu)."""
+"""Converts decimal numbers to binary by division.
+Thanks go to Eric Nygren (nygrene@stcsc.edu)."""
 
-__author__="Andrew Pennebaker (andrew.pennebaker@gmail.com)"
-__date__="9 Oct 2005 - 8 Nov 2005"
-__copyright__="Copyright 2005 Andrew Pennebaker"
+__author__ = "Andrew Pennebaker (andrew.pennebaker@gmail.com)"
+__date__ = "9 Oct 2005 - 8 Nov 2005"
+__copyright__ = "Copyright 2005 Andrew Pennebaker"
 
 def convert(decimal):
-	if decimal==0:
-		print("0000")
-		return
+  """Convert and print a decimal number in binary"""
 
-	binary=""
-	while decimal>0:
-		# append remainders in reverse order
-		binary="%d%s" % (decimal%2, binary)
-		decimal/=2
+  if decimal == 0:
+    print("0000")
+    return
 
-	# nibble form
-	if len(binary)%4!=0:
-		binary="%s%s" % ("0"*(4-len(binary)%4), binary)
+  binary = ""
+  while decimal > 0:
+    # append remainders in reverse order
+    binary = "%d%s" % (decimal % 2, binary)
+    decimal /= 2
 
-	print(binary)
+  # nibble form
+  if len(binary) % 4 != 0:
+    binary = "%s%s" % ("0" * (4 - len(binary) % 4), binary)
+
+  print(binary)
