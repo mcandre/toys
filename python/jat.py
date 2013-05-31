960df6391fff40aabcf7f37078a6a994fbdda645
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 21:43:40 -0400

    lint

--- python/jat.py
+++ python/jat.py
@@ -1,52 +1,65 @@
 #!/usr/bin/env python3
 
+"""ASCII art"""
+
 # JAT: The Joy of ASCII Art
 # http://codegolf.com/bob-ross-the-joy-of-ascii-art
 
 def chomp(s):
-	if len(s) > 0 and s[-1] == "\n":
-		return s[:-2]
-	else:
-		return s
+  """Strip trailing line endings"""
+
+  if len(s) > 0 and s[-1] == "\n":
+    return s[:-2]
+  else:
+    return s
 
 def get_input():
-	while True:
-		try:
-			line = input()
-			if len(line) == 0:
-				break
-			else:
-				yield chomp(line)
-		except EOFError:
-			break
+  """Read input from stdin"""
+
+  while True:
+    try:
+      line = raw_input()
+
+      if len(line) == 0:
+        break
+      else:
+        yield chomp(line)
+    except EOFError:
+      break
 
 def mark(rows, x, y, c):
-	space = " "
+  """Mark a region"""
 
-	rows += [] * (y + 1 - len(rows))
+  space = " "
 
-	rows[y] += space * (x + 1 - len(rows[y]))
+  rows += [] * (y + 1 - len(rows))
 
-	rows[y][x]=c
+  rows[y] += space * (x + 1 - len(rows[y]))
+
+  rows[y][x] = c
 
 def print_pic(rows):
-	for row in rows:
-		print("".join(row))
+  """Print ASCII art"""
+
+  for row in rows:
+    print("".join(row))
 
 def main():
-	rows = []
+  """CLI"""
 
-	for line in get_input():
-		parts = line.split(" ")
+  rows = []
 
-		x, y, c = int(parts[0]), int(parts[1]), chr(int(parts[2]))
+  for line in get_input():
+    parts = line.split(" ")
 
-		mark(rows, x, y, c)
+    x, y, c = int(parts[0]), int(parts[1]), chr(int(parts[2]))
 
-	print_pic(rows)
+    mark(rows, x, y, c)
 
-if __name__=="__main__":
-	try:
-		main()
-	except KeyboardInterrupt:
-		pass
+  print_pic(rows)
+
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
