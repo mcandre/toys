0ba599564b9f192122245d7850da475ed93281a4
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 23:39:15 -0400

    lint

--- python/opts.py
+++ python/opts.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
 
+"""Getopt demo"""
+
 import sys, getopt
 
 def usage():
-	print("Usage: %s [options]" % (sys.argv[0]))
-	print("-a ambulate")
-	print("-b bumble")
-	print("-c custom")
-	print("--run walk at a fast pace")
-	print("--float walk above the ground")
-	print("--Float <file> (float above a file)")
-	print("-u usage")
+  """Print usage message"""
 
-	sys.exit()
+  print("Usage: %s [options]" % (sys.argv[0]))
+  print("-a ambulate")
+  print("-b bumble")
+  print("-c custom")
+  print("--run walk at a fast pace")
+  print("--float walk above the ground")
+  print("--Float <file> (float above a file)")
+  print("-u usage")
+
+  sys.exit()
 
 def main():
-	optlist, args=getopt.getopt(sys.argv[1:], "abc:u", ["run", "float", "Float="])
+  """CLI"""
 
-	if len(optlist) < 1:
-		usage()
+  optlist, args = getopt.getopt(
+    sys.argv[1:],
+    "abc:u",
+    ["run", "float", "Float="]
+  )
 
-	for option, value in optlist:
-		if option == "-u":
-			usage()
-		elif option == "-a":
-			print("ambulating")
-		elif option == "-b":
-			print("bumbling")
-		elif option == "-c":
-			print(value)
-		elif option == "--run":
-			print("running")
-		elif option == "--float":
-			print("floating")
-		elif option == "--Float":
-			print("Floating above %s" % (value))
+  if len(optlist) < 1:
+    usage()
 
-	for arg in args:
-		print("additional argument %s" % (arg))
+  for option, value in optlist:
+    if option == "-u":
+      usage()
+    elif option == "-a":
+      print("ambulating")
+    elif option == "-b":
+      print("bumbling")
+    elif option == "-c":
+      print(value)
+    elif option == "--run":
+      print("running")
+    elif option == "--float":
+      print("floating")
+    elif option == "--Float":
+      print("Floating above %s" % (value))
+
+  for arg in args:
+    print("additional argument %s" % (arg))
 
 if __name__ == "__main__":
-	main()
+  main()
