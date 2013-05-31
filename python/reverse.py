6f480185fd42697f6efb34ad88a4c35f2521daf1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-31 11:01:10 -0400

    lint

--- python/reverse.py
+++ python/reverse.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
 
+"""Reverser"""
+
 import random
 
 def flip(sequence, howmany):
-	a = sequence[:howmany]
-	a.reverse()
+  """Flipper"""
 
-	return a + sequence[howmany:]
+  a = sequence[:howmany]
+  a.reverse()
+
+  return a + sequence[howmany:]
 
 def disordered(sequence):
-	for i in range(1, len(sequence) - 1):
-		if sequence[i - 1] > sequence[i]:
-			return True
+  """Unsorted?"""
 
-	return False
+  for i in range(1, len(sequence) - 1):
+    if sequence[i - 1] > sequence[i]:
+      return True
+
+  return False
+
+def main():
+  """CLI"""
+
+  inp = list(range(1, 10))
+  numbers = [inp.pop(random.randint(0, len(inp) - 1)) for i in range(len(inp))]
+
+  count = 0
+
+  while disordered(numbers):
+    print(" ".join([str(e) for e in numbers]))
+    howmany = int(raw_input("How many to flip? "))
+    howmany = int(howmany)
+    numbers = flip(numbers, howmany)
+    count += 1
+
+  print("Done! That took you %d steps." % (count))
 
 if __name__ == "__main__":
-	inp = list(range(1, 10))
-	numbers = [inp.pop(random.randint(0, len(inp)-1)) for i in range(len(inp))]
-
-	count = 0
-
-	while disordered(numbers):
-		print(" ".join([str(e) for e in numbers]))
-		howmany = input("How many to flip? ")
-		howmany = int(howmany)
-		numbers = flip(numbers, howmany)
-		count += 1
-
-	print("Done! That took you %d steps." % (count))
+  main()
