bef654d23b4f1519228990fc02aebca05d624a75
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-30 21:30:59 -0400

    lint

--- python/fractionfiles/fractionfiles.py
+++ python/fractionfiles/fractionfiles.py
@@ -3,164 +3,148 @@
 """Splits and joins files. Helpful when media can't fit a file.
 Be prepared for a lot of output files!"""
 
-__author__="Andrew Pennebaker (andrew.pennebaker@gmail.com)"
-__date__="6 Jan 3006 - 12 Feb 2006"
-__copyright__="Copyright 2006 Andrew Pennebaker"
-__version__="0.3"
-__URL__="http://snippets.dzone.com/posts/show/3541"
+__author__ = "Andrew Pennebaker (andrew.pennebaker@gmail.com)"
+__date__ = "6 Jan 3006 - 12 Feb 2006"
+__copyright__ = "Copyright 2006 Andrew Pennebaker"
+__version__ = "0.3"
+__URL__ = "http://snippets.dzone.com/posts/show/3541"
 
 import sys, os
-from getopt import getopt
+import getopt
 
-SPLIT_MODE="SPLIT"
-JOIN_MODE="JOIN"
+SPLIT_MODE = "SPLIT"
+JOIN_MODE = "JOIN"
 
-def splitFile(name, length, number):
-	if length==None:
-		infile=open(name, "rb")
-		size=0
-		while infile.read(1)!="":
-			size+=1
+def split_file(name, length, number):
+  """Split a file into specified parts"""
 
-		infile.close()
+  if length == None:
+    infile = open(name, "rb")
+    size = 0
+    while infile.read(1) != "":
+      size += 1
 
-		maxlength=size/number
-		if number*maxlength<size:
-			maxlength+=1
+    infile.close()
 
-	else:
-		if length<1:
-			raise Exception
+    maxlength = size / number
+    if number * maxlength < size:
+      maxlength += 1
+  else:
+    if length < 1:
+      raise Exception
 
-	infile=None
-	try:
-		infile=open(name, "rb")
-	except Exception, e:
-		raise e
+  infile = open(name, "rb")
 
-	i=0
-	j=0
-	c=infile.read(1)
-	while c!="":
-		outfile=open("%s.%d" % (name, j), "wb")
+  i = 0
+  j = 0
+  c = infile.read(1)
+  while c != "":
+    outfile = open("%s.%d" % (name, j), "wb")
 
-		while i<length and c!="":
-			outfile.write(c)
-			c=infile.read(1)
-			i+=1
+    while i < length and c != "":
+      outfile.write(c)
+      c = infile.read(1)
+      i += 1
 
-		outfile.close()
-		i=0
-		j+=1
+    outfile.close()
+    i = 0
+    j += 1
 
-	infile.close()
+  infile.close()
 
-def joinFiles(filenames):
-	if len(filenames)<1:
-		raise Exception
+def join_files(filenames):
+  """Join files back together"""
 
-	filenames.sort() # ...0 must be first
+  if len(filenames) < 1:
+    raise Exception
 
-	origFilename=filenames[0][0:-2] # take ".0" off the first file name
-	origFile=None
+  filenames.sort() # ...0 must be first
 
-	try:
-		origFile=open(origFilename, "wb")
-	except Exception, e:
-		raise e
+  original_filename = filenames[0][0:-2] # take ".0" off the first file name
 
-	c="&" # dummy
+  original_file = open(original_filename, "wb")
 
-	for filename in filenames:
-		smallFile=None
-		try:
-			smallFile=open(filename, "rb")
-		except Exception, e:
-			raise e
+  c = "&" # dummy
 
-		c=smallFile.read(1)
-		while c!="":
-			origFile.write(c)
-			c=smallFile.read(1)
+  for filename in filenames:
+    small_file = open(filename, "rb")
 
-		smallFile.close()
+    c = small_file.read(1)
+    while c != "":
+      original_file.write(c)
+      c = small_file.read(1)
 
-	origFile.close()
+    small_file.close()
+
+  original_file.close()
 
 def usage():
-	print "Usage: %s [options] [files]" % (sys.argv[0])
-	print "\n--split <file1 file 2 file 3...>"
-	print "--join <dir1 dir2 dir3 ...>"
-	print "--maxlength <bytes>"
-	print "--maxfiles <number>"
-	print "--help (usage)"
+  """Print usage message"""
 
-	sys.exit()
+  print "Usage: %s [options] [files]" % (sys.argv[0])
+  print "\n--split <file1 file 2 file 3...>"
+  print "--join <dir1 dir2 dir3 ...>"
+  print "--maxlength <bytes>"
+  print "--maxfiles <number>"
+  print "--help (usage)"
+
+  sys.exit()
 
 def main():
-	global SPLIT_MODE
-	global JOIN_MODE
+  """Split or join files as specified"""
 
-	mode=SPLIT_MODE
-	filenames=[]
-	maxlength=1024
-	maxfiles=None
+  mode = SPLIT_MODE
+  filenames = []
+  maxlength = 1024
+  maxfiles = None
 
-	systemArgs=sys.argv[1:] # ignore program name
+  system_args = sys.argv[1:] # ignore program name
 
-	optlist=[]
-	args=[]
+  optlist = []
+  args = []
 
-	try:
-		optlist, args=getopt(systemArgs, "", ["split", "join", "maxlength=", "maxfiles=", "help"])
-	except Exception, e:
-		usage()
+  try:
+    optlist, args = getopt.getopt(
+      system_args,
+      "",
+      ["split", "join", "maxlength=", "maxfiles=", "help"]
+    )
+  except getopt.GetoptError:
+    usage()
 
-	if len(optlist)<1 or len(args)<1:
-		usage()
+  if len(optlist)<1 or len(args)<1:
+    usage()
 
-	for option, value in optlist:
-		if option=="--help":
-			usage()
+  for option, value in optlist:
+    if option == "--help":
+      usage()
 
-		elif option=="--split":
-			mode=SPLIT_MODE
-		elif option=="--join":
-			mode=JOIN_MODE
-		elif option=="--maxlength":
-			try:
-				maxlength=int(value)
-				if maxlength<1:
-					raise Exception
-				maxfiles=None
-			except Exception:
-				raise Exception("Length must be at least one")
-		elif option=="--maxfiles":
-			try:
-				maxfiles=int(value)
-				if maxfiles<1:
-					raise Exception
-				maxlength=None
-			except Exception:
-				raise Exception("Number must be at least one")
+    elif option == "--split":
+      mode = SPLIT_MODE
+    elif option == "--join":
+      mode = JOIN_MODE
+    elif option == "--maxlength":
+      maxlength = int(value)
 
-	filenames=args
+      if maxlength < 1:
+        raise Exception("Length must be at least one")
+    elif option == "--maxfiles":
+      maxfiles = int(value)
 
-	if mode==SPLIT_MODE:
-		for filename in filenames:
-			try:
-				splitFile(filename, maxlength, maxfiles)
-			except Exception, e:
-				raise e
+      if maxfiles < 1:
+        raise Exception("Number must be at least one")
 
-	elif mode==JOIN_MODE:
-		for directory in filenames:
-			files=["%s%s%s" % (directory, os.sep, f) for f in os.listdir(directory)]
+  filenames = args
 
-			try:
-				joinFiles(files)
-			except Exception, e:
-				raise e
+  if mode == SPLIT_MODE:
+    for filename in filenames:
+      split_file(filename, maxlength, maxfiles)
 
-if __name__=="__main__":
-	main()
+  elif mode == JOIN_MODE:
+    for directory in filenames:
+      files = ["%s%s%s" % (directory, os.sep, f) for f in os.listdir(directory)]
+
+      join_files(files)
+
+if __name__ == "__main__":
+  main()
