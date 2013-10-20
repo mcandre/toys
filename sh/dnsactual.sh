c0c98b3115e04a6e69a84597715f135c84b5a980
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-10-20 16:53:54 -0400

    lint: sh conventions

--- sh/dnsactual.sh
+++ sh/dnsactual.sh
@@ -12,8 +12,8 @@
 ##############################################################################
 
 if test -f dnsactual.conf
-	then
-	CacheIP=$(cat dnsactual.conf)
+  then
+  CacheIP=$(cat dnsactual.conf)
 fi
 #echo $CacheIP
 
@@ -22,13 +22,13 @@ CurreIP=$(tail afraid.log | grep Detected | cut -d : -f 2 | cut -d '<' -f 1 | tr
 #echo $CurreIP
 if [ "$CurreIP" = "$CacheIP" ]
 then
-	# Both IP are equal
-	echo "Update not required..."
+  # Both IP are equal
+  echo "Update not required..."
 else
-	# The IP has change
-	echo "Updating http://free.afraid.org with " $CurreIP
-	wget http://freedns.afraid.org/dynamic/update.php?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -o /dev/null -O afraid.log
-	echo `date`  "Updating log with IP " $CurreIP >> dnsactual.log
+  # The IP has change
+  echo "Updating http://free.afraid.org with $CurreIP"
+  wget http://freedns.afraid.org/dynamic/update.php?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -o /dev/null -O afraid.log
+  echo `date` "Updating log with IP $CurreIP" >> dnsactual.log
 fi
 rm -f dnsactual.conf
-echo $CurreIP > dnsactual.conf
+echo "$CurreIP" > dnsactual.conf
