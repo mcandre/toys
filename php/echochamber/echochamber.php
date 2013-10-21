60f4a5d5d81d43df0b1723c373fd5f55b95fa90c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-10-20 23:43:28 -0400

    php lint

--- php/echochamber/echochamber.php
+++ php/echochamber/echochamber.php
@@ -3,17 +3,18 @@
 $text="";
 
 if (array_key_exists("text", $_GET)) {
-    $text=stripslashes($_GET["text"]);
+  $text=stripslashes($_GET["text"]);
 }
 
-$html = <<<HERE<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+$html = <<<HERE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <title>Echochamber</title>
-    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    <link href="stylesheet.css" media="screen" rel="stylesheet" type="text/css" />
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  <title>Echochamber</title>
+  <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+  <link href="stylesheet.css" media="screen" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -38,8 +39,8 @@ $html = <<<HERE<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "h
 HERE;
 
 if ($text=="") {
-    echo $html;
+  echo $html;
 } else {
-    echo $text;
+  echo $text;
 }
 ?>
