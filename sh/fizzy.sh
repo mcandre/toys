7946630dd2dcfb4c0d794f137eb5c68333a4fa2c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-10-26 10:33:36 -0400

    shellcheck: removed some expr warnings

--- sh/fizzy.sh
+++ sh/fizzy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 div3() {
-  expr $1 % 3 = 0
+  echo $((($1 % 3) == 0))
 }
 
 div5() {
-  expr $1 % 5 = 0
+  echo $((($1 % 5) == 0))
 }
 
 fizzy() {
-  if [ $(div3 $1) -eq 1 ] && [ $(div5 $1) -eq 1 ]; then
+  if [ "$(div3 "$1")" -eq 1 ] && [ "$(div5 "$1")" -eq 1 ]; then
     expr "FizzBuzz"
-  elif [ $(div3 $1) -eq 1 ]; then
+  elif [ "$(div3 "$1")" -eq 1 ]; then
     expr "Fizz"
-  elif [ $(div5 $1) -eq 1 ]; then
+  elif [ "$(div5 "$1")" -eq 1 ]; then
     expr "Buzz"
   else
     expr "$1"
