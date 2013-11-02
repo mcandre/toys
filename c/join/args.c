ce4161c207c92d868c5c148d431cfdc6d29e2375
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 18:30:21 -0400

    spacing

--- c/join/args.c
+++ c/join/args.c
@@ -3,11 +3,11 @@
 #include "join.h"
 
 int main(int argc, char** argv) {
-  char *arguments="";
+  char* arguments = "";
 
   int i;
-  for (i=0; i<argc; i++) {
-    arguments=join(arguments, argv[i], " ");
+  for (i = 0; i < argc; i++) {
+    arguments = join(arguments, argv[i], " ");
   }
 
   printf("Arguments: %s\n", arguments);
