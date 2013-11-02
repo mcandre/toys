b43dcb2b081362eae210b4b76e8c27102e64c51b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 14:15:53 -0400

    ghc warnings

--- haskell/maths.hs
+++ haskell/maths.hs
@@ -1,5 +1,5 @@
 -- Compile:
--- ghc -O2 --make maths.hs -threaded -rtsopts
+-- ghc -O2 --make maths.hs -threaded -rtsopts -Wall
 --
 -- Run:
 -- time ./maths +RTS -N2
@@ -7,19 +7,23 @@
 import Text.Printf
 import Control.Parallel
 
+main :: IO ()
 main = (a::Integer) `par` (b::Integer) `par` (c::Integer) `pseq` printf "A = %d\nB = %d\nC = %d\n" a b c
   where
     a = ack 3 10
     b = fac 42
     c = fib 34
 
+fac :: Integer -> Integer
 fac 0 = 1
 fac n = n * fac (n-1)
 
+ack :: Integer -> Integer -> Integer
 ack 0 n = n + 1
 ack m 0 = ack (m-1) 1
 ack m n = ack (m-1) (ack m (n-1))
 
+fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
