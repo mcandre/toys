2c721c57e74742d947df8e9c351d030736fe6f78
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 22:55:46 -0400

    checkstyle and Xlint

--- java/Swatch.java
+++ java/Swatch.java
@@ -5,15 +5,22 @@
 
 import java.util.Calendar;
 
-public class Swatch {
-  public static int getTimezone(Calendar c) {
-    return -(c.get(c.ZONE_OFFSET) + c.get(c.DST_OFFSET)) / (60 * 1000)/60;
+public final class Swatch {
+  private Swatch() {}
+
+  public static int getTimezone(final Calendar c) {
+    return -(
+      c.get(Calendar.ZONE_OFFSET) +
+      c.get(Calendar.DST_OFFSET)
+    ) / (60 * 1000) / 60;
   }
 
   public static double beat() {
     Calendar c = Calendar.getInstance();
 
-    int hour = c.get(c.HOUR_OF_DAY) + getTimezone(c), min = c.get(c.MINUTE), sec = c.get(c.SECOND);
+    int hour = c.get(Calendar.HOUR_OF_DAY) + getTimezone(c);
+    int min = c.get(Calendar.MINUTE);
+    int sec = c.get(Calendar.SECOND);
 
     int utc = hour * 3600 + min * 60 + sec; // Greenwich, England
 
@@ -21,8 +28,9 @@ public class Swatch {
 
     double beat = bmt / 86.4;
 
-    if (beat > 1000)
+    if (beat > 1000) {
       beat -= 1000;
+    }
 
     return beat;
   }
@@ -31,7 +39,7 @@ public class Swatch {
     return String.format("@%06.2f", beat());
   }
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     System.out.println(swatch());
   }
 }
