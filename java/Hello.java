2ca33613b00b1a756bc076153fe6087b2fe35218
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 22:47:36 -0400

    checkstyle

--- java/Hello.java
+++ java/Hello.java
@@ -1,5 +1,7 @@
-class Hello {
-  public static void main(String[] args) {
+public final class Hello {
+  private Hello() {}
+
+  public static void main(final String[] args) {
     System.out.println("Hello World");
   }
 }
