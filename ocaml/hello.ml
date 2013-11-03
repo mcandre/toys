05e2112e2c503c2a7d4576004e576f493d7a07d8
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-03 00:50:23 -0400

    extra ocaml warnings

--- ocaml/hello.ml
+++ ocaml/hello.ml
@@ -1,8 +1,20 @@
-if true then ignore begin let kkkk _ _ _ _ = 0 in kkkk
-"exec" "ocaml" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
+if true then ignore begin let kkkk _ _ _ _ _ _ = 0 in kkkk
+"exec" "ocaml" "-w" "+a" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
 then exit
 fi
 true else 0
 end;;
+
+(*
+
+Compile
+
+ocamlc -w +a -o hello hello.ml
+
+Run
+
+./hello
+
+*)
 
 let main = print_endline "Hello World!"
