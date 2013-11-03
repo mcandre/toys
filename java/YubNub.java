46407e3dadb3214251307ee40d63127f11ce775e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 22:58:43 -0400

    checkstyle and Xlint

--- java/YubNub.java
+++ java/YubNub.java
@@ -1,25 +1,22 @@
 import java.io.IOException;
 
-public class YubNub {
-  public static final String parser="http://yubnub.org/parser/parse?command=";
+public final class YubNub {
+  private YubNub() {}
 
-  public static void go(String command) {
-    try {
-      Runtime.getRuntime().exec("open "+parser+command);
-    }
-    catch(IOException e) {
-      e.printStackTrace();
-    }
+  public static final String PARSER = "http://yubnub.org/parser/parse?command=";
+
+  public static void go(final String command) throws IOException {
+    Runtime.getRuntime().exec("open " + PARSER + command);
   }
 
-  public static void main(String[] args) {
-    if (args.length<1) {
+  public static void main(final String[] args) throws IOException {
+    if (args.length < 1) {
       go("");
     }
     else {
-      String command=args[0];
-      for (int i=1; i<args.length; i++) {
-        command+="+"+args[i];
+      String command = args[0];
+      for (int i = 1; i < args.length; i++) {
+        command += "+" + args[i];
       }
 
       go(command);
