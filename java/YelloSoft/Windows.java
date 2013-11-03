c7dd574b2749ec2526064544b7ae518fb079ae56
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 22:45:57 -0400

    checkstyle

--- java/YelloSoft/Windows.java
+++ java/YelloSoft/Windows.java
@@ -2,8 +2,10 @@ import java.awt.Window;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-public class Windows {
-  public static void centerOnScreen(Window window) {
+public final class Windows {
+  private Windows() {}
+
+  public static void centerOnScreen(final Window window) {
     Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
     window.setLocation(
       (dimension.width - window.getSize().width) / 2,
