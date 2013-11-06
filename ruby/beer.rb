2f8bd724afd88cb7dad784ca505d5fb1a3a5223a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-05 21:50:17 -0500

    contracts

--- ruby/beer.rb
+++ ruby/beer.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
-99.downto(3) { |i|
-  puts "#{i} bottles of beer on the wall, #{i} bottles of beer.\nTake one down, pass it around, #{i-1} bottles of beer on the wall.\n\n"
+require "contracts"
+include Contracts
+
+Contract Num => String
+def beer(i)
+  if i >= 3 then
+    "#{i} bottles of beer on the wall, #{i} bottles of beer.\nTake one down, pass it around, #{i-1} bottles of beer on the wall.\n\n"
+  elsif i > 1 then
+    "2 bottles of beer on the wall, 2 bottle of beer.\nTake one down, pass it around, 1 bottle of beer on the wall.\n\n"
+  else
+    "1 bottle of beer on the wall, 1 bottle of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+  end
+end
+
+99.downto(1) { |i|
+  puts beer(i)
 }
-
-puts "2 bottles of beer on the wall, 2 bottle of beer.\nTake one down, pass it around, 1 bottle of beer on the wall.\n\n"
-
-puts "1 bottle of beer on the wall, 1 bottle of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
