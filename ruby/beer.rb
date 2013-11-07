27c9603a6742158479672d1b91a7dc32fbefe429
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-06 22:54:53 -0500

    cane

--- ruby/beer.rb
+++ ruby/beer.rb
@@ -6,11 +6,14 @@ include Contracts
 Contract Num => String
 def self.beer(i)
   if i >= 3 then
-    "#{i} bottles of beer on the wall, #{i} bottles of beer.\nTake one down, pass it around, #{i-1} bottles of beer on the wall.\n\n"
+    "#{i} bottles of beer on the wall, #{i} bottles of beer.\n" +
+      "Take one down, pass it around, #{i-1} bottles of beer on the wall.\n\n"
   elsif i > 1 then
-    "2 bottles of beer on the wall, 2 bottle of beer.\nTake one down, pass it around, 1 bottle of beer on the wall.\n\n"
+    "2 bottles of beer on the wall, 2 bottle of beer.\n" +
+      "Take one down, pass it around, 1 bottle of beer on the wall.\n\n"
   else
-    "1 bottle of beer on the wall, 1 bottle of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
   end
 end
 
