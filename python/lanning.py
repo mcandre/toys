0581cad1df8a52383565c82712cfe37d20c7dc16
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-09 18:12:05 -0500

    comment: how to install AppKit

--- python/lanning.py
+++ python/lanning.py
@@ -25,7 +25,12 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+#
+# Requires AppKit
+# https://pypi.python.org/pypi/AppKit/
+#
+# pip install AppKit
+#
 # I, Robot Transcript
 # http://www.script-o-rama.com/movie_scripts/i/i-robot-script-transcript.html
 #
