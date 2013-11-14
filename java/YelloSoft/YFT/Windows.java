b227a1d03fe04f6f8e7fe057bbb87fca6d97da43
Andrew Pennebaker <apennebaker@42six.com>
2013-11-14 12:37:43 -0500

    checkstyle

--- java/YelloSoft/YFT/Windows.java
+++ java/YelloSoft/YFT/Windows.java
@@ -1,11 +1,15 @@
-import java.awt.*;
+import java.awt.Window;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-public class Windows {
-  public static void centerOnScreen(Window window) {
-    Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
+public final class Windows {
+  private Windows() {}
+
+  public static void centerOnScreen(final Window window) {
+    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
     window.setLocation(
-      (dimension.width-window.getSize().width)/2,
-      (dimension.height-window.getSize().height)/2
+      (dimension.width - window.getSize().width) / 2,
+      (dimension.height - window.getSize().height) / 2
     );
   }
 }
