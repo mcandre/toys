6d0f5bfb89592b826e834bf4d60ea35290b37c62
Andrew Pennebaker <apennebaker@42six.com>
2013-12-12 11:12:46 -0500

    checkstyle

--- java/YelloSoft/KeyGen/SecretKeyFilter.java
+++ java/YelloSoft/KeyGen/SecretKeyFilter.java
@@ -1,11 +1,19 @@
+//
 // SecretKeyFilter
 // Andrew Pennebaker
 // June 12, 2004
+//
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
+/** Secret key file selector */
 public class SecretKeyFilter extends FileFilter {
+  /**
+     <p>Accept only secret key files</p>
+     @param f file
+     @return acceptibility
+  */
   public final boolean accept(final File f) {
     // display all directories
     if (f.isDirectory()) {
@@ -18,11 +26,19 @@ public class SecretKeyFilter extends FileFilter {
     return extension.equals("ser");
   }
 
+  /**
+     <p>Get description</p>
+     @return description
+  */
   public final String getDescription() {
     return "Secret keys";
   }
 
-  // get extension of a file (lowercase)
+  /**
+     <p>Get file extension</p>
+     @param f file
+     @return file extension (lowercase)
+  */
   private String getExtension(final File f) {
     String s = f.getName();
     int i = s.lastIndexOf('.');
