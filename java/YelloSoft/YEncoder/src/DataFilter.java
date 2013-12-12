0c728f623864fd7c530445ef1b51705c98c3ec67
Andrew Pennebaker <apennebaker@42six.com>
2013-12-12 11:00:09 -0500

    checkstyle

--- java/YelloSoft/YEncoder/src/DataFilter.java
+++ java/YelloSoft/YEncoder/src/DataFilter.java
@@ -1,11 +1,19 @@
+//
 // DataFilter
 // Andrew Pennebaker
 // June 10, 2004
+//
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
+/** Data filter */
 public class DataFilter extends FileFilter {
+  /**
+     <p>Select data files</p>
+     @param f file
+     @return whether the file is acceptable
+  */
   public final boolean accept(final File f) {
     // display all directories
     if (f.isDirectory()) {
@@ -18,11 +26,19 @@ public class DataFilter extends FileFilter {
     return extension.equals("dat") || extension.equals("");
   }
 
+  /**
+     <p>Get description</p>
+     @return description
+  */
   public final String getDescription() {
     return "Data files";
   }
 
-  // get extension of a file (lowercase)
+  /**
+     <p>Get file extension</p>
+     @param f file
+     @return file extension (lowercase)
+  */
   private String getExtension(final File f) {
     String s = f.getName();
     int i = s.lastIndexOf('.');
