96bbfb3eae91bdcfe485b7bc67d6475fb3098324
Andrew Pennebaker <apennebaker@42six.com>
2013-12-12 10:22:38 -0500

    checkstyle

--- java/YelloSoft/YFT/YByteOutputStream.java
+++ java/YelloSoft/YFT/YByteOutputStream.java
@@ -1,16 +1,27 @@
+//
 // YByteOutputStream
 // Andrew Pennebaker
 // July 7, 2004
+//
 
 import java.io.*;
 
+/** Mock of a byte output stream */
 public class YByteOutputStream extends ByteArrayOutputStream {
+  /**
+     <p>Construct a mock</p>
+     @param newBuf buffer
+  */
   public YByteOutputStream(final byte[] newBuf) {
     super();
 
     buf = newBuf;
   }
 
+  /**
+     <p>Get buffer</p>
+     @return buffer
+  */
   public final byte[] getBytes() {
     return buf;
   }
