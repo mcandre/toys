8c5f719ee25b8d61589109d06441d7db9f32efe0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-02-14 14:55:15 -0500

    echo

---
+++ ruby/echo.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def main
+  c = STDIN.getc
+
+  while not c.nil?
+    putc c
+    c = STDIN.getc
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  begin
+    main
+  rescue Interrupt
+    nil
+  end
+end
