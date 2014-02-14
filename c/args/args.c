f18d525685924c23d52b96a9dd2fa715944673fe
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-02-14 13:34:32 -0500

    args

---
+++ c/args/args.c
@@ -0,0 +1,12 @@
+// Copyright 2014 YelloSoft
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int i;
+  for (i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+
+  return 0;
+}
