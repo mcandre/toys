f63a6cd303089099faf17d765b21bd6a284da84b
Andrew Pennebaker <apennebaker@42six.com>
2014-03-06 11:57:56 -0500

    spelling

---
+++ groovy/toucan.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+final def toucan = 2
+
+println "How many can ride on a toucan?"
+
+final def answer = String.format("%d can.", toucan)
+
+println answer
--- groovy/tucan.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-final def tucan = 2
-
-println "How many can ride on a tucan?"
-
-final def answer = String.format("%d can.", tucan)
-
-println answer
