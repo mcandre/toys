c8c60fe36738e7322f990f56b1b08682cf013f42
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-07-22 10:50:18 -0500

    lint

---
+++ haskell/petstore/Petstore.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/env runhaskell
+
+module Petstore where
+
+import qualified Cat as C
+import qualified Dog as D
+
+blaspheme :: C.Cat -> D.Dog
+blaspheme (C.Cat name weight owner) = D.Dog name weight owner
+
+main :: IO ()
+main = do
+  let jerry = C.Cat "Jerry" 5 "Tom"
+  let rover = D.Dog "Rover" 10 "Col. Husky"
+
+  putStrLn $ D.name (blaspheme jerry)
+  putStrLn $ D.name rover
