9f587d85754d02f2f10d24062d924d4e427144a9
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-12 10:33:55 -0500

    strings

--- ruby/beer.rb
+++ ruby/beer.rb
@@ -9,14 +9,18 @@ def self.beer(i)
     "#{i} bottles of beer on the wall, #{i} bottles of beer.\n" +
       "Take one down, pass it around, #{i - 1} bottles of beer on the wall.\n\n"
   elsif i > 1
-    '2 bottles of beer on the wall, 2 bottle of beer.\n' +
-      'Take one down, pass it around, 1 bottle of beer on the wall.\n\n'
+    "2 bottles of beer on the wall, 2 bottle of beer." +
+      "Take one down, pass it around, 1 bottle of beer on the wall.\n\n"
   else
-    '1 bottle of beer on the wall, 1 bottle of beer.\n' +
-      'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
   end
 end
 
-99.downto(1) do |i|
-  puts beer(i)
+def main
+  99.downto(1) do |i|
+    puts beer(i)
+  end
 end
+
+main if $PROGRAM_NAME == __FILE__
