a8cb7ecd456d66ea887e3b9938840961b6e139fd
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-12 10:46:19 -0500

    contracts

--- ruby/beer.rb
+++ ruby/beer.rb
@@ -3,24 +3,24 @@
 require 'contracts'
 include Contracts
 
-Contract Num => String
-def self.beer(i)
-  if i >= 3
-    "#{i} bottles of beer on the wall, #{i} bottles of beer.\n" +
-      "Take one down, pass it around, #{i - 1} bottles of beer on the wall.\n\n"
-  elsif i > 1
-    "2 bottles of beer on the wall, 2 bottle of beer." +
-      "Take one down, pass it around, 1 bottle of beer on the wall.\n\n"
-  else
-    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-      "Go to the store and buy some more, 99 bottles of beer on the wall."
+module Beer
+  Contract Num => String
+  def self.beer(i)
+    if i >= 3
+      "#{i} bottles of beer on the wall, #{i} bottles of beer.\n" +
+        "Take one down, pass it around, #{i - 1} bottles of beer on the wall.\n\n"
+    elsif i > 1
+      "2 bottles of beer on the wall, 2 bottle of beer." +
+        "Take one down, pass it around, 1 bottle of beer on the wall.\n\n"
+    else
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall."
+    end
   end
 end
 
 def main
-  99.downto(1) do |i|
-    puts beer(i)
-  end
+  99.downto(1).collect { |i| Beer.beer i }.each { |line| puts line }
 end
 
 main if $PROGRAM_NAME == __FILE__
