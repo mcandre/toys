5e734659938470a5afe0ab15cb6da81f6858d69f
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-13 14:07:06 -0500

    cleanup grunt

--- node/Gruntfile.js
+++ node/Gruntfile.js
@@ -1,14 +1,12 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 		exec: {
-			test: "npm test",
       jshint: "jshint ."
 		}
   });
 
   grunt.loadNpmTasks("grunt-exec");
 
-  grunt.registerTask("default", ["exec:test"]);
-  grunt.registerTask("test", ["exec:test"]);
+  grunt.registerTask("default", ["exec:jshint"]);
   grunt.registerTask("lint", ["exec:jshint"]);
 };
