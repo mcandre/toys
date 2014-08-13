59306313bfa01313dfe7be9528a70e155544d351
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-13 14:50:28 -0500

    grunt

---
+++ coffeescript/Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+		exec: {
+      coffeelint: "coffeelint -q .",
+      tidy: "find . -type d -name node_modules -prune -o -type f -name '*.html' -print -exec tidy -qe {} \\; 2>&1 | grep -v 'canvas>' | grep -v 'proprietary attribute'; true"
+		}
+  });
+
+  grunt.loadNpmTasks("grunt-exec");
+
+  grunt.registerTask("default", ["exec:coffeelint", "exec:tidy"]);
+  grunt.registerTask("lint", ["exec:coffeelint", "exec:tidy"]);
+};
