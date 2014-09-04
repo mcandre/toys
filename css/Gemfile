caef473d6cc5955377ce45610284334efd381f15
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-04 18:10:47 -0500

    scss-lint

---
+++ css/Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+group :development do
+  gem 'scss-lint', '~> 0.28.0'
+end
--- css/Gruntfile.js
+++ css/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     exec: {
       csslint: "csslint .",
+      scss_lint: "scss-lint .",
       tidy: "find . -type d -name node_modules -prune -o -type f -name '*.html' -print -exec tidy -qe {} \\; 2>&1 | grep -v 'canvas>' | grep -v 'proprietary attribute'; true"
     }
   });
@@ -9,7 +10,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-exec");
 
   grunt.registerTask("default", ["exec:csslint", "exec:tidy"]);
-  grunt.registerTask("lint", ["exec:csslint", "exec:tidy"]);
+  grunt.registerTask("lint", ["exec:csslint", "exec:scss_lint", "exec:tidy"]);
+  grunt.registerTask("scss_lint", ["exec:scss_lint"]);
   grunt.registerTask("csslint", ["exec:csslint"]);
   grunt.registerTask("tidy", ["exec:tidy"]);
 };
