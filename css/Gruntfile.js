09907d7059c30864453644099439b39f9375dbf6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-04 14:35:15 -0500

    lint

--- css/Gruntfile.js
+++ css/Gruntfile.js
@@ -1,13 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     exec: {
-      jshint: "jshint .",
+      csslint: "csslint .",
       tidy: "find . -type d -name node_modules -prune -o -type f -name '*.html' -print -exec tidy -qe {} \\; 2>&1 | grep -v 'canvas>' | grep -v 'proprietary attribute'; true"
     }
   });
 
   grunt.loadNpmTasks("grunt-exec");
 
-  grunt.registerTask("default", ["exec:jshint", "exec:tidy"]);
-  grunt.registerTask("lint", ["exec:jshint", "exec:tidy"]);
+  grunt.registerTask("default", ["exec:csslint", "exec:tidy"]);
+  grunt.registerTask("lint", ["exec:csslint", "exec:tidy"]);
+  grunt.registerTask("csslint", ["exec:csslint"]);
+  grunt.registerTask("tidy", ["exec:tidy"]);
 };
