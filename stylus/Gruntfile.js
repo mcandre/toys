e7187cace3c6e1f09ae98e68ab685cbbd0dff8d3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-04 17:13:33 -0500

    grunt

---
+++ stylus/Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    exec: {
+      hello: "stylus hello.styl",
+      fizzy: "stylus fizzy.styl",
+      tidy: "find . -type d -name node_modules -prune -o -type f -name '*.html' -print -exec tidy -qe {} \\; 2>&1 | grep -v 'canvas>' | grep -v 'proprietary attribute'; true",
+      clean: "rm *.css; exit 0"
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-exec");
+
+  grunt.registerTask("default", ["exec:hello", "exec:fizzy"]);
+  grunt.registerTask("hello", ["exec:hello"]);
+  grunt.registerTask("fizzy", ["exec:fizzy"]);
+  grunt.registerTask("lint", ["exec:tidy"]);
+  grunt.registerTask("tidy", ["exec:tidy"]);
+  grunt.registerTask("clean", ["exec:clean"]);
+};
