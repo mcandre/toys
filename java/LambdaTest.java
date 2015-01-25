649ab305e9e9b3c7f49457c5ea16c33f5ce5ae97
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-01-25 01:08:43 -0600

    java 8 test program

---
+++ java/LambdaTest.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class LambdaTest {
+  public static void main(String[] args) {
+    ArrayList<Integer> xsList = new ArrayList<Integer>();
+
+    for (Integer i = 0; i < 10; i++) {
+      xsList.add(i);
+    }
+
+    Stream<Integer> xsParallelStream = xsList.stream().parallel();
+
+    List<Integer> ys = xsParallelStream.map( (Integer i) -> i * 2 ).collect(Collectors.toList());
+
+    for (Integer y: ys) {
+      System.out.println(y);
+    }
+  }
+}
