3094eb9b3fac239566e4ea1a582bd7009367a7dd
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-03-24 14:04:58 -0500

    astyle ObjC, but not MATLAB/Octave

--- objc/fizzy/Fizzy.m
+++ objc/fizzy/Fizzy.m
@@ -2,17 +2,15 @@
 #import "Fizzy.h"
 
 @implementation Fizzy
-+ (NSString*) fizzy: (const NSInteger) n {
++ (NSString *) fizzy: (const NSInteger) n {
   const BOOL div3 = (n % 3 == 0);
   const BOOL div5 = (n % 5 == 0);
 
   if (div3 && div5) {
     return @"FizzBuzz";
-  }
-  else if (div3) {
+  } else if (div3) {
     return @"Fizz";
-  }
-  else if (div5) {
+  } else if (div5) {
     return @"Buzz";
   }
 
