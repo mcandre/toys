1946b19af9c336c9b441afc75af2adc5edf3a634
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-04-01 11:31:18 -0500

    eslint

--- javascript/Gruntfile.js
+++ javascript/Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
     exec: {
       jshint: 'node_modules/jshint/bin/jshint .',
       jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/jslint/bin/jslint.js {} \\;',
+      eslint: "node_modules/eslint/bin/eslint.js .",
       tidy: 'find . -type d -name node_modules -prune -o -type f -name "*.html" -print -exec tidy -qe {} \\; 2>&1 | grep -v "canvas>" | grep -v "proprietary attribute"; true'
     }
   });
@@ -16,10 +17,12 @@ module.exports = function(grunt) {
     'exec:jshint',
     // Go to bed, Crockford
     // 'exec:jslint',
+    "exec:eslint",
     'exec:tidy'
   ]);
- 
+
   grunt.registerTask('jshint', ['exec:jshint']);
+  grunt.registerTask("eslint", ["exec:eslint"]);
   grunt.registerTask('jslint', ['exec:jslint']);
-  grunt.registerTask('tidy', ['exec:tidy']); 
+  grunt.registerTask('tidy', ['exec:tidy']);
 };
