8f7dddd0329d2986c1b9f4c25364f15f047d82fd
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-30 11:01:16 -0500

    pep8

--- bf/bs.py
+++ bf/bs.py
@@ -15,109 +15,113 @@ import getopt
 tape = [0] * 100
 address = 0
 
+
 def sublevel(level):
-  i = 0
+    i = 0
 
-  if level.count("[") > level.count("]"):
-    raise Exception("Unmatched Bracket")
+    if level.count("[") > level.count("]"):
+        raise Exception("Unmatched Bracket")
 
-  while level[0:i + 1].count("[") != level[0:i + 1].count("]"):
-    i += 1
+    while level[0:i + 1].count("[") != level[0:i + 1].count("]"):
+        i += 1
 
-  return level[1:i + 1]
+    return level[1:i + 1]
 
-def run(code, position = 0):
-  global tape
-  global address
 
-  while position < len(code):
-    cmd = code[position]
+def run(code, position=0):
+    global tape
+    global address
 
-    if cmd == "<":
-      address -= 1
-    elif cmd == ">":
-      address += 1
-    elif cmd == "+":
-      tape[address] += 1
-    elif cmd == "-":
-      tape[address] -= 1
-    elif cmd == "!":
-      sys.stdout.write(chr(tape[address]))
-    elif cmd == "?":
-      try:
-        tape[address] = ord(sys.stdin.read(1))
-      except:
-        tape[address] -= 1
-    elif cmd == "[":
-      try:
-        level = sublevel(code[position:])
+    while position < len(code):
+        cmd = code[position]
 
-        while tape[address] != 0:
-          run(level)
+        if cmd == "<":
+            address -= 1
+        elif cmd == ">":
+            address += 1
+        elif cmd == "+":
+            tape[address] += 1
+        elif cmd == "-":
+            tape[address] -= 1
+        elif cmd == "!":
+            sys.stdout.write(chr(tape[address]))
+        elif cmd == "?":
+            try:
+                tape[address] = ord(sys.stdin.read(1))
+            except:
+                tape[address] -= 1
+        elif cmd == "[":
+            try:
+                level = sublevel(code[position:])
 
-        position += len(level)
-      except:
+                while tape[address] != 0:
+                    run(level)
+
+                position += len(level)
+            except:
+                return position
+        elif cmd == "]":
+            return position
+
+        position += 1
+
         return position
-    elif cmd == "]":
-      return position
 
-    position += 1
-
-    return position
 
 def usage():
-  print("Usage: %s [options] [<sourcefile>]" % (sys.argv[0]))
-  print("-h|--help (usage)")
+    print("Usage: %s [options] [<sourcefile>]" % (sys.argv[0]))
+    print("-h|--help (usage)")
 
-  sys.exit()
+    sys.exit()
+
 
 def main():
-  systemArgs = sys.argv[1:]
+    systemArgs = sys.argv[1:]
 
-  live = False
+    live = False
 
-  optlist, args = [], []
+    optlist, args = [], []
 
-  try:
-    optlist, args = getopt.getopt(systemArgs, "h", ["help"])
-  except Exception:
-    usage()
+    try:
+        optlist, args = getopt.getopt(systemArgs, "h", ["help"])
+    except Exception:
+        usage()
 
-  live = len(args) < 1
+    live = len(args) < 1
 
-  for option, value in optlist:
-    if option == "-h" or option == "--help":
-      usage()
+    for option, value in optlist:
+        if option == "-h" or option == "--help":
+            usage()
 
-  if live:
-    print("--BF Interpreter 0.5--")
-    print("  Type exit to exit.")
+    if live:
+        print("--BF Interpreter 0.5--")
+        print("    Type exit to exit.")
 
-    code = ""
-    position = 0
+        code = ""
+        position = 0
 
-    line = "not exit"
+        line = "not exit"
 
-    while line != "exit":
-      sys.stdout.write("% ")
-      line = sys.stdin.readline().rstrip(os.linesep)
-      code += line
+        while line != "exit":
+            sys.stdout.write("% ")
+            line = sys.stdin.readline().rstrip(os.linesep)
+            code += line
 
-      position = run(code, position)
-  else:
-    src = args[0]
-
-    srcfile = open(src, "r")
-    code = "".join(srcfile.readlines())
-    srcfile.close()
-
-    if code.count("[") != code.count("]"):
-      raise Exception("Unbalanced brackets")
+            position = run(code, position)
     else:
-      run(code)
+        src = args[0]
+
+        srcfile = open(src, "r")
+        code = "".join(srcfile.readlines())
+        srcfile.close()
+
+        if code.count("[") != code.count("]"):
+            raise Exception("Unbalanced brackets")
+        else:
+            run(code)
 
 if __name__ == "__main__":
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
