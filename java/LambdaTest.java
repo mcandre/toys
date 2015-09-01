234280cf36ac7ac19a8f043fda259a2b8da1a392
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-09-01 13:25:16 -0500

    immutable variables

--- java/LambdaTest.java
+++ java/LambdaTest.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 public class LambdaTest {
   public static void main(String[] args) {
-    ArrayList<Integer> xsList = new ArrayList<Integer>();
+    final ArrayList<Integer> xsList = new ArrayList<Integer>();
 
     for (Integer i = 0; i < 10; i++) {
       xsList.add(i);
     }
 
-    Stream<Integer> xsParallelStream = xsList.stream().parallel();
+    final Stream<Integer> xsParallelStream = xsList.stream().parallel();
 
-    List<Integer> ys = xsParallelStream
+    final List<Integer> ys = xsParallelStream
       .map((Integer i) -> i * 2)
       .collect(Collectors.toList());
 
-    for (Integer y : ys) {
+    for (final Integer y : ys) {
       System.out.println(y);
     }
   }
