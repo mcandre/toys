13c42d9b0b98c7f025a260a9cbdfad3f38aac671
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-12-12 12:14:49 -0600

    parallel FizzBuzz in Java 8

---
+++ java/fizzy/src/main/java/us/yellosoft/fizzy/Fizzy.java
@@ -0,0 +1,34 @@
+package us.yellosoft.fizzy;
+
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
+/** Example FizzBuzz implementation, parallelized */
+public class Fizzy {
+  /** Utility class */
+  private Fizzy() {}
+
+  public static String fizzbuzz(int i) {
+    boolean m = i % 3 == 0, n = i % 5 == 0;
+
+    if (m && n) {
+      return "FizzBuzz";
+    } else if (m) {
+      return "Fizz";
+    } else if (n) {
+      return "Buzz";
+    } else {
+      return String.valueOf(i);
+    }
+  }
+
+  /** CLI entry point
+      @param args CLI flags (unused)
+   */
+  public static void main(final String[] args) {
+    final IntStream xs = IntStream.range(1, 101).parallel();
+    final String[] ys = xs.mapToObj(Fizzy::fizzbuzz).toArray(String[]::new);
+
+    System.out.println(Arrays.toString(ys));
+  }
+}
