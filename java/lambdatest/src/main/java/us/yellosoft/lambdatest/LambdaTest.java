7d7f274ab2c2a9dcb7a0d0a0e7fe783ff88c03ed
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-12-12 11:59:42 -0600

    javadoc

--- java/lambdatest/src/main/java/us/yellosoft/lambdatest/LambdaTest.java
+++ java/lambdatest/src/main/java/us/yellosoft/lambdatest/LambdaTest.java
@@ -3,11 +3,15 @@ package us.yellosoft.lambdatest;
 import java.util.stream.IntStream;
 import java.util.Arrays;
 
+/** Example use of Java 8 lambdas */
 public class LambdaTest {
   /** Utility class */
   private LambdaTest() {}
 
-  public static void main(String[] args) {
+  /** CLI entry point
+      @param args CLI flags (unused)
+   */
+  public static void main(final String[] args) {
     final IntStream xs = IntStream.range(0, 10).parallel();
     final int[] ys = xs.map((int i) -> i * 2).toArray();
 
