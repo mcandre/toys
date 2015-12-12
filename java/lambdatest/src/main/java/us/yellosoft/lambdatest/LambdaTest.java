758c4d52647a631315e2480aeec3ea56bbb8505b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-12-12 11:58:50 -0600

    brevity

--- java/lambdatest/src/main/java/us/yellosoft/lambdatest/LambdaTest.java
+++ java/lambdatest/src/main/java/us/yellosoft/lambdatest/LambdaTest.java
@@ -1,31 +1,16 @@
 package us.yellosoft.lambdatest;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class LambdaTest {
   /** Utility class */
   private LambdaTest() {}
 
   public static void main(String[] args) {
-    final ArrayList<Integer> xsList = new ArrayList<Integer>();
+    final IntStream xs = IntStream.range(0, 10).parallel();
+    final int[] ys = xs.map((int i) -> i * 2).toArray();
 
-    for (Integer i = 0; i < 10; i++) {
-      xsList.add(i);
-    }
-
-    final Stream<Integer> xsParallelStream = xsList.stream().parallel();
-
-    final List<Integer> ys = xsParallelStream
-      .map((Integer i) -> i * 2)
-      .collect(Collectors.toList());
-
-    for (final Integer y : ys) {
-      System.out.println(y);
-    }
-
-    System.exit(0);
+    System.out.println(Arrays.toString(ys));
   }
 }
