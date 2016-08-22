aa7c935d22bd9b91c5ea72854b9bbd35c9161bf0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-08-22 14:57:34 -0500

    test dup.go from 'The Go Programming Language' book

---
+++ go/dup/dup.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+)
+
+func main() {
+  frequencies := make(map[string]int)
+
+  scanner := bufio.NewScanner(os.Stdin)
+
+  for scanner.Scan() {
+    frequencies[scanner.Text()]++
+  }
+
+  for line, frequency := range frequencies {
+    fmt.Printf("%d\t%s\n", frequency, line)
+  }
+}
