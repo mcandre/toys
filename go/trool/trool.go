cf1758bfd327bc8fdbd99219c2aa14e1a7048e2a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-08-31 12:34:12 -0500

    more Go struct, interface, enum examples

---
+++ go/trool/trool.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Trool string
+
+const (
+	TriTrue Trool = "TriTrue"
+	TriFalse      = "TriFalse"
+	TriMeh        = "TriMeh"
+)
+
+func main() {
+	t1 := TriTrue
+	t2 := TriFalse
+	t3 := TriMeh
+
+	fmt.Println(t1)
+	fmt.Println(t2)
+	fmt.Println(t3)
+}
