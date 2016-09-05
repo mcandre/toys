d76a20ba19aa8b7f1553fa80b8407d26c7cf02f7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-09-05 01:16:14 -0500

    fix logic

--- go/magic/main.go
+++ go/magic/main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 )
 
-var data = []byte{0x09, 0xf9, 0x11, 0x02, 0x9d, 0x74, 0xe3, 0x5b, 0xd8, 0x41, 0x56, 0xc5, 0x63, 0x56, 0x88, 0xbd}
+var data = [17]byte{0x09, 0xf9, 0x11, 0x02, 0x9d, 0x74, 0xe3, 0x5b, 0xd8, 0x41, 0x56, 0xc5, 0x63, 0x56, 0x88, 0xbd}
 
 func main() {
-	var data2  []byte
-	var pieces []string
+	var data2 [16]byte
+	var pieces [17]string
 
 	for i := 0; i < 6; i++ {
 		if i != 3 {
-			copy(data2, data)
-			data2[len(data2)-1] += byte(i)
-			for i, e := range data2 {
-				pieces[i] = fmt.Sprintf("%02x", e)
+			copy(data2[:], data[:])
+			data2[15] += byte(i)
+
+			for k, v := range data2 {
+				pieces[k] = fmt.Sprintf("%02x", v)
 			}
 
 			fmt.Println(strings.Join(pieces[:], " "))
