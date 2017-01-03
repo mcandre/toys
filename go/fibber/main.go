f63f0a7969293794942bf67e4594b2c682e77108
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 11:04:39 -0600

    godoc

--- go/fibber/main.go
+++ go/fibber/main.go
@@ -1,3 +1,4 @@
+// Package main provides a fibber executable for demonstrating a basic Fibonacci implementation in Go.
 package main
 
 import (
@@ -6,11 +7,13 @@ import (
 	"strconv"
 )
 
+// usage prints the command line interface for this application.
 func usage() {
 	fmt.Println("fibber <n>")
 	os.Exit(0)
 }
 
+// fib calculates the Fibonacci function for a given non-negative integer.
 func fib(n uint) uint {
 	switch n {
 	case 0, 1:
@@ -20,6 +23,7 @@ func fib(n uint) uint {
 	}
 }
 
+// main is the entrypoint for this application.
 func main() {
 	if len(os.Args) < 2 {
 		usage()
