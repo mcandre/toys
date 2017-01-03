e02760e14b53b775da590691942d0db241917715
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 11:14:18 -0600

    godoc

--- go/magic/main.go
+++ go/magic/main.go
@@ -1,3 +1,4 @@
+// Package main provides a magic executable that leaks HD DVD keys.
 package main
 
 import (
@@ -5,8 +6,10 @@ import (
 	"strings"
 )
 
+// data is the reverse engineered HD DVD key.
 var data = [...]byte{0x09, 0xf9, 0x11, 0x02, 0x9d, 0x74, 0xe3, 0x5b, 0xd8, 0x41, 0x56, 0xc5, 0x63, 0x56, 0x88, 0xbd}
 
+// main is the entrypoint for this application.
 func main() {
 	var data2 [16]byte
 	var pieces [16]string
