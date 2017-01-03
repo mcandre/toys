66f9b8fcd8c2eeab2b7ec9e9c79343fd5caca218
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 11:09:33 -0600

    godoc

--- go/hello/main.go
+++ go/hello/main.go
@@ -1,7 +1,9 @@
+// Package main provides a hello executable demonstrating a Go Hello World implementation.
 package main
 
 import "fmt"
 
+// main is the entrypoint for this application.
 func main() {
 	fmt.Println("Hello World!")
 }
