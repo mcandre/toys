6a6364587c9fec8c24b8c11f79bde3eacd786fc7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 10:53:50 -0600

    godoc

--- go/99bottles/main.go
+++ go/99bottles/main.go
@@ -1,7 +1,9 @@
+// Package main provides a 99bottles executable.
 package main
 
 import "fmt"
 
+// main is the entrypoint for this application.
 func main() {
 	for i := 99; i > 1; i-- {
 		fmt.Printf("%d bottles of beer on the wall,\n%d bottles of beer!\nTake one down, pass it around,\n%d bottles of beer on the wall!\n", i, i, i-1)
