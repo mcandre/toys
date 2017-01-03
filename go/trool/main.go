cba4c29dda9c9836b95d725986411145a50e2b56
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 11:19:51 -0600

    godoc

--- go/trool/main.go
+++ go/trool/main.go
@@ -1,15 +1,19 @@
+// Package main provides a trool executable for demonstrating declaring Go types.
 package main
 
 import "fmt"
 
+// Trool is like a boolean, but with one additional mode: Meh.
 type Trool string
 
+// Trool constants are declared in terms of string values: "TrieTrue", "TriFalse", and "TriMeh".
 const (
 	TriTrue  Trool = "TriTrue"
 	TriFalse       = "TriFalse"
 	TriMeh         = "TriMeh"
 )
 
+// main is the entrypoint for this application.
 func main() {
 	t1 := TriTrue
 	t2 := TriFalse
