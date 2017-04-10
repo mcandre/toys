bfed4f6ddc59284895325f40fa509d7deec3b4a8
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-04-10 00:02:42 -0500

    fix rust syntax for 1.16.0

--- rust/args/args.rs
+++ rust/args/args.rs
@@ -1,12 +1,9 @@
 //! Command line arguments (argv)
 
-use std::os::args;
-use std::vec::Vec;
+use std::env;
 
 fn main() {
-  let argv : Vec<String> = args();
-
-  for arg in argv.iter() {
-    println!("{}", *arg);
+  for arg in env::args() {
+    println!("{}", arg);
   }
 }
