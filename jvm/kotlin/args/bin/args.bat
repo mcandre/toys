9b6b0c5e783a595cef5069e9551bd4c3f7e20b7a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-01-23 11:05:08 -0600

    add CLI processing example for kotlin

---
+++ jvm/kotlin/args/bin/args.bat
@@ -0,0 +1,1 @@
+@java -jar "%~dpn0"\..\build\libs\args.jar %*
---
+++ jvm/kotlin/args/src/main/kotlin/us/yellosoft/args/Args.kt
@@ -0,0 +1,9 @@
+@file:JvmName("Args")
+
+package us.yellosoft.args
+
+fun main(args: Array<String>) {
+    for (arg in args) {
+        println(arg)
+    }
+}
