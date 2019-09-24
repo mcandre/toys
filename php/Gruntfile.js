7b8ddb12b573986c208a8b74be92fac3280438ec
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-09-24 15:41:57 -0500

    jsfmt

--- php/Gruntfile.js
+++ php/Gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         exec: {
+            jsfmt: 'find . -type d -name node_modules -prune -o -type f -name "*[.-]min.js" -prune -o \\( -type f -name "*.js" -o -type f -path "*/bin/*" \\) -exec node node_modules/.bin/jsfmt -w {} \\;',
             phplint: 'find . -name "*.php" -exec php -l {} \\; | grep -v "No syntax errors detected"; true',
             csslint: 'csslint .',
             scssLint: 'scss-lint .',
@@ -13,6 +14,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('default', [
+        'exec:jsfmt',
         'exec:phplint',
         'exec:csslint',
         'exec:scssLint',
@@ -20,12 +22,14 @@ module.exports = function(grunt) {
     ]);
 
     grunt.registerTask('lint', [
+        'exec:jsfmt',
         'exec:phplint',
         'exec:csslint',
         'exec:scssLint',
         'exec:tidy'
     ]);
 
+    grunt.registerTask('jsfmt', ['exec:jsfmt']);
     grunt.registerTask('phplint', ['exec:phplint']);
     grunt.registerTask('csslint', ['exec:csslint']);
     grunt.registerTask('scssLint', ['exec:scssLint']);
