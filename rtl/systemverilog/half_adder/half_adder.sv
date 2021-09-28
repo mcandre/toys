85abbe3ede016c24000fac1946772f2d2b195f06
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-28 09:53:10 -0500

    drop dead code

--- rtl/systemverilog/half_adder/half_adder.sv
+++ rtl/systemverilog/half_adder/half_adder.sv
@@ -1,11 +1,10 @@
 module half_adder(
-    input clk,
-    a,
-    b,
+    input a,
+    input b,
     output reg sum,
-    carry
+    output reg carry
 );
-    always @(posedge clk)
+    always @*
         begin
             sum <= a ^ b;
             carry <= a & b;
@@ -13,9 +12,6 @@ module half_adder(
 endmodule
 
 module test_half_adder;
-    reg clk = 0;
-    always #1 clk = !clk;
-
     reg a = 0,
         b = 0;
     always #2 b = !b;
@@ -24,13 +20,12 @@ module test_half_adder;
     wire sum,
          carry;
 
-    half_adder ha(clk, a, b, sum, carry);
+    half_adder ha(a, b, sum, carry);
 
     initial
         $monitor(
-            "Time: %t, clk: %0d, a: %0d, b: %0d, sum: %0d, carry: %0d",
+            "Time: %t, a: %0d, b: %0d, sum: %0d, carry: %0d",
             $time,
-            clk,
             a,
             b,
             sum,
