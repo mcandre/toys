ab025015868812d6cd8d403056eb57c706164681
Andrew <andrew.pennebaker@gmail.com>
2021-09-28 10:04:54 -0500

    rtl

---
+++ rtl/systemverilog/half_adder/half_adder.sv
@@ -0,0 +1,26 @@
+module half_adder(input a, b, output sum, carry);
+    assign sum = a ^ b;
+    assign carry = a & b;
+endmodule
+
+module test_half_adder;
+    reg a = 0,
+        b = 0;
+    always #2 b = !b;
+    always #4 a = !a;
+
+    wire sum,
+         carry;
+
+    half_adder ha(a, b, sum, carry);
+
+    initial
+        $monitor(
+            "Time: %t, a: %0d, b: %0d, sum: %0d, carry: %0d",
+            $time,
+            a,
+            b,
+            sum,
+            carry
+        );
+endmodule
