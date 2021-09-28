d7181262bd2d0b2ae64a00e03dcf28874d5c0822
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-28 12:49:24 -0500

    refactor

--- rtl/systemverilog/half_adder/test_half_adder.sv
+++ rtl/systemverilog/half_adder/test_half_adder.sv
@@ -1,29 +1,21 @@
 `include "half_adder.sv"
 
 module test_half_adder;
-    reg a00 = 0,
-        b00 = 0;
     wire sum00,
          carry00;
-    half_adder ha00(a00, b00, sum00, carry00);
+    half_adder ha00(1'b0, 1'b0, sum00, carry00);
 
-    reg a01 = 0,
-        b01 = 1;
     wire sum01,
          carry01;
-    half_adder ha01(a01, b01, sum01, carry01);
+    half_adder ha01(1'b0, 1'b1, sum01, carry01);
 
-    reg a10 = 1,
-        b10 = 0;
     wire sum10,
          carry10;
-    half_adder ha10(a10, b10, sum10, carry10);
+    half_adder ha10(1'b1, 1'b0, sum10, carry10);
 
-    reg a11 = 1,
-        b11 = 1;
     wire sum11,
          carry11;
-    half_adder ha11(a11, b11, sum11, carry11);
+    half_adder ha11(1'b1, 1'b1, sum11, carry11);
 
     initial begin
         #1 assert (sum00 == 0);
