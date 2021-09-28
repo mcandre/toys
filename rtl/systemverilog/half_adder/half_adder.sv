3184ab2af51a3f31597c34ca0dce705f620ed03a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-28 09:55:50 -0500

    style

--- rtl/systemverilog/half_adder/half_adder.sv
+++ rtl/systemverilog/half_adder/half_adder.sv
@@ -1,10 +1,4 @@
-module half_adder(
-    input a,
-    b,
-    output sum,
-    carry
-);
-
+module half_adder(input a, b, output sum, carry);
     assign sum = a ^ b;
     assign carry = a & b;
 endmodule
