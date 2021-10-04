682f7bec845198da8d6958c648536dcb6acfcdcb
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-04 13:54:18 -0500

    debug

--- rtl/systemverilog/wholeass/wholeass.sv
+++ rtl/systemverilog/wholeass/wholeass.sv
@@ -8,12 +8,14 @@ endmodule
 module adder #(parameter N = 2)(input carry_in, [N-1:0] a, [N-1:0] b, output carry_out, [N-1:0] sum);
     wholeass wa[N];
 
-    for (int i = 1; i < N; i++) {
-        assign wa[i].carry_in = wa[i-1].carry_out;
-        assign wa[i].a = a[i];
-        assign wa[i].b = b[i];
-        assign sum[i] = wa[i].sum;
-    }
+    generate
+        for (int i = 1; i < N; i++) begin
+            assign wa[i].carry_in = wa[i-1].carry_out;
+            assign wa[i].a = a[i];
+            assign wa[i].b = b[i];
+            assign sum[i] = wa[i].sum;
+        end
+    endgenerate
 
     assign wa[0].carry_in = carry_in;
     assign carry_out = wa[N-1].carry_out;
