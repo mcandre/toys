0087a9ab25bbe7d5c48f2cbf3bc6d9eac97dd303
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-04 14:00:34 -0500

    debug

--- rtl/systemverilog/wholeass/test_wholeass.sv
+++ rtl/systemverilog/wholeass/test_wholeass.sv
@@ -1,17 +1,34 @@
 `include "wholeass.sv"
 
 module test_wholeass;
+    parameter N = 3;
+
     reg c = 0,
         carry_in = 0;
-    reg [2:0] a = 0,
-        [2:0] b = 0;
+    reg [N-1:0] a = 0,
+                b = 0;
     always #64 c = !c;
     always #8 a = a + 1;
     always #1 b = b + 1;
 
-    wire [2:0] sum;
     wire carry_out;
-    adder<3> wa(carry_in, a, b, carry_out, sum);
+    wire [N-1:0] sum;
+
+    wholeass wa[N];
+    genvar i;
+
+    generate
+        for (i = 1; i < N; i++) begin
+            assign wa[i].carry_in = wa[i-1].carry_out;
+            assign wa[i].a = a[i];
+            assign wa[i].b = b[i];
+            assign sum[i] = wa[i].sum;
+        end
+    endgenerate
+
+    assign wa[0].carry_in = carry_in;
+    assign sum[0] = wa[0].sum;
+    assign carry_out = wa[N-1].carry_out;
 
     always #1 begin
         #0 begin
--- rtl/systemverilog/wholeass/wholeass.sv
+++ rtl/systemverilog/wholeass/wholeass.sv
@@ -4,19 +4,3 @@ module wholeass(input carry_in, a, b, output carry_out, sum);
                        (a & carry_in) |
                        (b & carry_in);
 endmodule
-
-module adder #(parameter N = 2)(input carry_in, [N-1:0] a, [N-1:0] b, output carry_out, [N-1:0] sum);
-    wholeass wa[N];
-
-    generate
-        for (int i = 1; i < N; i++) begin
-            assign wa[i].carry_in = wa[i-1].carry_out;
-            assign wa[i].a = a[i];
-            assign wa[i].b = b[i];
-            assign sum[i] = wa[i].sum;
-        end
-    endgenerate
-
-    assign wa[0].carry_in = carry_in;
-    assign carry_out = wa[N-1].carry_out;
-endmodule
