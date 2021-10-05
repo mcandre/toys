4d8149f52dcba29f51174e0f8d0fe5db56f585ca
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-04 21:30:30 -0500

    select more specific verilator annotation

--- rtl/systemverilog/wholeass/test_wholeass.sv
+++ rtl/systemverilog/wholeass/test_wholeass.sv
@@ -1,5 +1,3 @@
-/* verilator lint_off UNOPTFLAT */
-
 `include "wholeass.sv"
 
 module test_wholeass;
@@ -9,7 +7,7 @@ module test_wholeass;
     reg carry_in /* verilator public */;
     wire carry_out /* verilator public */;
     wire [N-1:0] sum /* verilator public */;
-    wire [N-2:0] carries;
+    wire [N-2:0] carries /* verilator split_var */;
     wholeass wa_head(carry_in, a[0], b[0], carries[0], sum[0]);
     wholeass wa_tail(carries[N-2], a[N-1], b[N-1], carry_out, sum[N-1]);
     genvar i;
