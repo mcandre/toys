67b95afcbdfaa2b0f7c0778649504221d0663e1f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-09 17:56:19 -0500

    c++ idioms

--- rtl/systemverilog/halfass/main.cpp
+++ rtl/systemverilog/halfass/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            Vtest_halfass *top = new Vtest_halfass{"top"};
+            auto *top = new Vtest_halfass{"top"};
             top->test_halfass->a = i;
             top->test_halfass->b = j;
             top->eval();
