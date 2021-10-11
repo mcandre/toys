a04e395fc42420c0908c16dfd91b7c8b40143a1c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-11 13:24:02 -0500

    refactor

--- rtl/systemverilog/halfadder/main.cpp
+++ rtl/systemverilog/halfadder/main.cpp
@@ -1,26 +1,19 @@
 #include <cstdlib>
 
-#include "Vtest_halfadder.h"
-#include "Vtest_halfadder__Syms.h"
+#include "Vhalfadder.h"
+#include "Vhalfadder__Syms.h"
 
 int main(int argc, char **argv) {
-    int carry = 0;
-
     Verilated::commandArgs(argc, argv);
+    Vhalfadder top{"top"};
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            Vtest_halfadder top{"top"};
-            auto *ha = top.test_halfadder;
-            ha->a = i;
-            ha->b = j;
+            top.a = i;
+            top.b = j;
             top.eval();
-            carry = ha->carry;
-            assert(carry == (i & j));
-
-            if (!carry) {
-                assert(ha->sum == (i ^ j));
-            }
+            assert(top.carry == (i & j));
+            assert(top.sum == (i ^ j));
         }
     }
 
--- rtl/systemverilog/halfadder/test_halfadder.sv
+++
@@ -1,9 +0,0 @@
-`include "halfadder.sv"
-
-module test_halfadder;
-    reg a /* verilator public */,
-        b /* verilator public */;
-    wire sum /* verilator public */,
-        carry /* verilator public */;
-    halfadder ha(a, b, sum, carry);
-endmodule
