1a400e7a73ecc8cbe1d4ba5b98081846812a6759
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-10-11 10:57:02 -0500

    ignore spurious simulator warnings

--- rtl/systemverilog/fulladder/fulladder_chain.sv
+++ rtl/systemverilog/fulladder/fulladder_chain.sv
@@ -1,7 +1,7 @@
 `include "fulladder.sv"
 
 module fulladder_chain #(parameter WIDTH)(input carry_in, [WIDTH-1:0] a, [WIDTH-1:0] b, output carry_out, [WIDTH-1:0] sum);
-    wire [WIDTH-2:0] carries /* verilator split_var */;
+    wire [WIDTH-2:0] carries;
     fulladder fa_head(carry_in, a[0], b[0], carries[0], sum[0]);
     fulladder fa_tail(carries[WIDTH-2], a[WIDTH-1], b[WIDTH-1], carry_out, sum[WIDTH-1]);
     genvar i;
--- rtl/systemverilog/fulladder/main.cpp
+++ rtl/systemverilog/fulladder/main.cpp
@@ -4,18 +4,23 @@
 #include "Vtest_fulladder_chain__Syms.h"
 
 int main(int argc, char **argv) {
+    const int width = 8;
+    const int min = 0;
+    const int len = 1 << width;
+    const int max = len - 1;
+
     Verilated::commandArgs(argc, argv);
 
-    for (int i = 0; i < 256; i++) {
-        for (int j = 0; j < 256; j++) {
+    for (int i = min; i < len; i++) {
+        for (int j = min; j < len; j++) {
             Vtest_fulladder_chain top{"top"};
             auto *fa = top.test_fulladder_chain;
             fa->a = i;
             fa->b = j;
             fa->carry_in = 0;
             top.eval();
-            assert(fa->sum == ((i + j) % 256));
-            assert(fa->carry_out == ((i + j) > 255));
+            assert(fa->carry_out == ((i + j) > max));
+            assert(fa->sum == ((i + j) % len));
         }
     }
 
