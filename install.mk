50311dc8fcabf96036f8ea6eda6a3d57c0461f7c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-04-27 02:15:51 -0500

    update unmake

--- install.mk
+++ install.mk
@@ -1,13 +1,11 @@
 .PHONY: all
 
 all:
-	cargo install --force \
-		crit@0.0.6 \
-		unmake@0.0.6
+	@cargo install --force unmake@0.0.7
 
-	go install github.com/mcandre/stank/...@v0.0.24
-	go install mvdan.cc/sh/v3/cmd/shfmt@latest
-	go mod tidy
+	@go install github.com/mcandre/stank/...@v0.0.24
+	@go install mvdan.cc/sh/v3/cmd/shfmt@latest
+	@go mod tidy
 
-	pip3 install --upgrade pip setuptools
-	pip3 install -r requirements-dev.txt
+	@pip3 install --upgrade pip setuptools
+	@pip3 install -r requirements-dev.txt
